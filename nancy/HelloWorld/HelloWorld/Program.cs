1b7a58af0709a7e51c112a5e69090b54d373f6e5
Igor <igor.zc@gmail.com>
2014-10-22 18:18:46 +0300

    added nancy sample

---
+++ nancy/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class SampleModule : Nancy.NancyModule
+    {
+        public SampleModule()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
