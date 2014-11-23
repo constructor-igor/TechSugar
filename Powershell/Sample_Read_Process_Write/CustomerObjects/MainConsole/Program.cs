a226d0940cc6ac23ef688ca0a75423e49ee022fd
Igor <igor.zc@gmail.com>
2014-11-24 00:03:54 +0200

    minor

--- Powershell/Sample_Read_Process_Write/CustomerObjects/MainConsole/Program.cs
+++ Powershell/Sample_Read_Process_Write/CustomerObjects/MainConsole/Program.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks.Dataflow;
 using Customer.DataProcessing;
 using Customer.TextData;
