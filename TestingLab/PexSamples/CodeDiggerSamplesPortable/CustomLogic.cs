e344e123b2d09091aea6048e8e0fee70fc822a53
Igor <igor.zc@gmail.com>
2014-12-17 11:56:26 +0200

    added "Code digger" (PEX) samples

---
+++ TestingLab/PexSamples/CodeDiggerSamplesPortable/CustomLogic.cs
@@ -0,0 +1,34 @@
+﻿
+//
+//
+//  References:
+//  - http://research.microsoft.com/en-us/projects/codedigger/gettingstarted.aspx
+//  - http://research.microsoft.com/en-us/projects/pex/
+//
+
+using System;
+
+namespace CodeDiggerSamplesPortable
+{
+    public class CustomLogic
+    {
+        public int Calc(int x, int y)
+        {
+            if (x < 0)
+                return -1;
+            if (y < 0)
+                return -2;
+            if (x == 0 && y == 0)
+                return 0;
+            return 1;
+        }
+        public void ConverMe(int[] a)
+        {
+            if (a == null)
+                return;
+            if (a.Length>0)
+                if (a[3]==12345)
+                    throw new Exception("bug");
+        }
+    }
+}
