18d33d55e05f904b9450566ec0ca006f4119a49d
Igor <igor.zc@gmail.com>
2014-12-23 10:00:57 +0200

    added source file

---
+++ PluginFramework/FrameworksLab1/EngineAPI/Interfaces/IMaterialPropertiesService.cs
@@ -0,0 +1,9 @@
+﻿using Plugin.Framework.Interfaces;
+
+namespace EngineAPI.Interfaces
+{
+    public interface IMaterialPropertiesService : IService
+    {
+        double[] CalcRange(IModelDataEntity modelDataEntity, string materialName, double[] generalRange);
+    }
+}
