07abbcc3ef5627831b34e2449c06dce2e832f5f9
Igor <igor.zc@gmail.com>
2015-01-26 15:00:07 +0200

    added Pex demo

---
+++ TestingLab/AutoFixtureLab/PexDemo/PexDemoClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PexDemo
+{
+    public class PexDemoClass
+    {
+        public void ConvertMe(int[] a)
+        {
+            if (a == null)
+                return;
+            if (a.Length>0)
+                if (a[3] == 1234)
+                    throw new Exception("bug");
+        }
+    }
+}
