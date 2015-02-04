a485ceb116eb847e0b60d60aa9c47edacbb39555
Igor <igor.zc@gmail.com>
2015-02-04 09:28:12 +0200

    added IoC lab

---
+++ IoC/UnityLab/UnityLab/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace UnityLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UnityContainer unityContainer = new UnityContainer())
+            {
+                unityContainer.RegisterInstance(new Instance());
+            }
+            Console.WriteLine("unityContainer disposed");
+        }
+    }
+    public class Instance : IDisposable
+    {
+        public Instance()
+        {
+            Console.WriteLine("Instance.ctor");
+        }
+        #region IDisposable
+        public void Dispose()
+        {
+            Console.WriteLine("Instance.Dispose");
+        }
+        #endregion
+    }
+}
