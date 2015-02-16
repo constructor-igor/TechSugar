4dd0730a42dd545cd2922fd80e6bfeb4c3d988d9
Igor <igor.zc@gmail.com>
2015-02-16 15:41:39 +0200

    first SignalR sample implemented: console host - html client

---
+++ SignalR/SignalR-Intro/SelfHostConsole/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+/*
+ * 
+ * References:
+ * http://www.asp.net/signalr/overview/deployment/tutorial-signalr-self-host
+ * http://stackoverflow.com/questions/16004375/signalr-how-to-call-net-client-method-from-server
+ * */
+using Owin;
+
+namespace SelfHostConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 or http://+:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+
+            using (WebApp.Start<Startup>("http://localhost:8080/"))
+            {
+                Console.WriteLine("Server running at http://localhost:8080/");
+                Console.ReadLine();
+
+                IHubContext context = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+
+                string message = null;
+                do
+                {
+                    Console.Write("Enter message: ");
+                    message = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(message))
+                    {
+                        context.Clients.All.addMessage("server", message);
+                    }
+                } while (!string.IsNullOrEmpty(message));
+
+                Console.WriteLine("to close the serve, please, press Enter");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Console.WriteLine("{0}: {1}", name, message);
+            Clients.All.addMessage(name, message);
+        }
+    }
+}
