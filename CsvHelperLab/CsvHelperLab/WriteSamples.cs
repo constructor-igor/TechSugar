be0f0dda08c233fe127b2755945c8751147b002e
Igor <igor.zc@gmail.com>
2015-03-01 17:01:31 +0200

    added failed test

--- CsvHelperLab/CsvHelperLab/WriteSamples.cs
+++ CsvHelperLab/CsvHelperLab/WriteSamples.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using CsvHelper;
 using CsvHelper.Configuration;
 using NFile.Framework;
@@ -13,14 +15,10 @@ namespace CsvHelperLab
         [Test]
         public void WriteConstantParametersNumber()
         {
-            List<DataItem> dataItems = new List<DataItem>
-            {
-                new DataItem {FirstId = "10", SecondId = "20"},
-                new DataItem {FirstId = "10.1", SecondId = "20.1"}
-            };
+            List<DataItem> dataItems = CreateDataItems();
 
             using (TemporaryFile csvFile = new TemporaryFile())
-            {
+            {                
                 using (TextWriter textWriter = new StreamWriter(csvFile.FileName))
                 {
                     CsvConfiguration configuration = new CsvConfiguration {Delimiter = " "};
@@ -35,8 +33,66 @@ namespace CsvHelperLab
                 Assert.That(actualContent, Is.EquivalentTo(expectedContent));
             }
         }
+
+        [Test]
+        public void WriteIncludeVersionLine()
+        {
+            DataHeader header = new DataHeader {Version = "Ver=1"};
+            DataObject dataObject = new DataObject { Version = header, Items = CreateDataItems() };
+            using (TemporaryFile csvFile = new TemporaryFile())
+            {
+                using (TextWriter textWriter = new StreamWriter(csvFile.FileName))
+                {
+                    CsvConfiguration configuration = new CsvConfiguration { Delimiter = " " };
+                    using (CsvWriter csvWriter = new CsvWriter(textWriter, configuration))
+                    {
+                        csvWriter. WriteRecord(dataObject);
+                    }
+                }
+
+                List<string> expectedContent = new List<string> { "Ver=1", "FirstId SecondId", "10 20", "10.1 20.1" };
+                IEnumerable<string> actualContent = File.ReadLines(csvFile.FileName);
+                Assert.That(actualContent, Is.EquivalentTo(expectedContent));
+            }
+        }
+
+        #region private methods
+        private static List<DataItem> CreateDataItems()
+        {
+            List<DataItem> dataItems = new List<DataItem>
+            {
+                new DataItem {FirstId = "10", SecondId = "20"},
+                new DataItem {FirstId = "10.1", SecondId = "20.1"}
+            };
+            return dataItems;
+        }
+        #endregion
+
+        public sealed class DataObjectMap : CsvClassMap<DataObject>
+        {
+            public DataObjectMap()
+            {
+                Map(m => m.Version);
+                Map(m => m.Items);
+            }
+        }
     }
 
+    public class DataObject
+    {
+        public DataHeader Version { get; set; }
+        public List<DataItem> Items { get; set; }
+
+        public DataObject()
+        {
+            Items = new List<DataItem>();
+        }
+    }
+
+    public class DataHeader
+    {
+        public string Version { get; set; }
+    }
     public class DataItem
     {
         public string FirstId { get; set; }
