d3052d34817c89da10fa2ec345f1db7c3c3928dd
Igor <igor.zc@gmail.com>
2015-03-04 10:37:18 +0200

    added workaround

--- MEF/MEF/MEF-loading-issue/Program.cs
+++ MEF/MEF/MEF-loading-issue/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
 using System.Linq;
+using System.Reflection;
 using Plugin.Command.Interfaces;
 
 namespace MEF_loading_issue
@@ -10,14 +11,25 @@ namespace MEF_loading_issue
     /*
      * Project created from issue http://stackoverflow.com/questions/28836436/error-when-try-load-plugin-using-mef
      * 
+     * same issues
+     * http://stackoverflow.com/questions/11164797/compositioncontainer-loading-wrong-directory-through-directorycatalog
+     * section "Assembly Load Issues" http://blogs.msdn.com/b/dsplaisted/archive/2010/07/13/how-to-debug-and-diagnose-mef-failures.aspx
      * */
     class Program
     {
         static void Main()
         {
-            var client = new Client();
-            client.LoadPlugins();
-            Console.WriteLine("plugins count: {0}", client.CommandPlugins.Count());
+            try
+            {
+                var client = new Client();
+                client.LoadPlugins();
+                Console.WriteLine("plugins count: {0}", client.CommandPlugins.Count());
+            }
+            catch (Exception)
+            {
+                    
+                throw;
+            }
         }
     }
 
@@ -33,11 +45,14 @@ namespace MEF_loading_issue
             //var pluginDirectoryCatalog = new DirectoryCatalog(@"..\..\..\@PluginBinaries", "*.dll");
             //aggregateCatalog.Catalogs.Add(pluginDirectoryCatalog);
 
-            var pluginAssemblyCatalog = new AssemblyCatalog(@"..\..\..\@PluginBinaries\PluginAdd.dll");            
+            var pluginAssemblyCatalog = new AssemblyCatalog(@"..\..\..\@PluginBinaries\PluginAdd.dll");
+            // possible workaround: load plugin assembly and create AssemblyCatalog with help of loaded assembly (not by file path)
+            //var pluginAssemblyCatalog = new AssemblyCatalog(Assembly.LoadFrom(@"..\..\..\@PluginBinaries\PluginAdd.dll"));
             aggregateCatalog.Catalogs.Add(pluginAssemblyCatalog);
 
             var container = new CompositionContainer(aggregateCatalog);
             container.ComposeParts(this);
         }
     }
+    // try to load via catalog.Catalogs.Add(new AssemblyCatalog(Assembly.LoadFrom(@"C:\yosra\project\project.Plugin.Nav\bin\Debug\NavPlugin.dll")));
 }
