b84088150fe58a15fbef67867235a3ee20a65b06
Igor Ziselman <igor.zc@gmail.com>
2015-06-10 08:48:33 +0300

    Demo: "lock" solution

--- Actors/AkkaNet/AkkaNetSamples/Demo/Worker.cs
+++ Actors/AkkaNet/AkkaNetSamples/Demo/Worker.cs
@@ -7,6 +7,7 @@ namespace SingleImplementation
         private readonly MailService m_mailService;
         private readonly Repository m_repository;
         private int m_count;
+        static readonly object LockedObject = new object();
 
         public Worker()
         {
@@ -16,11 +17,14 @@ namespace SingleImplementation
 
         public string Run(string request)
         {
-            string message = String.Format("{0}: {1}", m_count, request);
-            m_count = m_count + 1;
-            m_repository.Save(message);
-            m_mailService.Send(message);
-            return message;
+            lock (LockedObject)
+            {
+                string message = String.Format("{0}: {1}", m_count, request);
+                m_count = m_count + 1;
+                m_repository.Save(message);
+                m_mailService.Send(message);
+                return message;
+            }
         }
     }
 }
