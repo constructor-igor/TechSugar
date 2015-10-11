771c617208dc3d617e77c2103c2fda69aef54058
constructor-igor <igor.zc@gmail.com>
2015-10-11 12:55:19 +0300

    updated test ProblemDefinition

--- IoC/UnityLab/UnityTests/UnitDisposeSamples.cs
+++ IoC/UnityLab/UnityTests/UnitDisposeSamples.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Practices.ObjectBuilder2;
+using Microsoft.Practices.Unity;
 using NUnit.Framework;
 
 namespace UnityTests
@@ -10,5 +9,77 @@ namespace UnityTests
     [TestFixture]
     public class UnitDisposeSamples
     {
+        [Test]
+        public void ProblemDefinition()
+        {
+            using (IUnityContainer unityContainer = new UnityContainer())
+            {
+                unityContainer.RegisterType<IService, BaseService>();
+                unityContainer.RegisterType<IService, FacadeService>("facade");
+
+                Enumerable.Range(0, 2).ForEach(i =>
+                {
+                    using (IUnityContainer childContainer = unityContainer.CreateChildContainer())
+                    {
+                        childContainer.RegisterType<IService, BaseService>(new ContainerControlledLifetimeManager());
+
+                        IService service = childContainer.Resolve<IService>("facade");
+                        service.Foo(String.Format("problemDefinitionTest({0})", i));
+                        Console.WriteLine("before childContainer.Dispose()");
+                    }
+                });
+
+                Console.WriteLine("before unityContainer.Dispose()");
+            }
+        }
+    }
+
+    public interface IService
+    {
+        string Foo(string message);
+    }
+
+    public class BaseService : IService, IDisposable
+    {
+        public BaseService()
+        {
+            Console.WriteLine("Created BaseService");
+        }
+        #region IService
+        public string Foo(string message)
+        {
+            Console.WriteLine("    BaseService({0}", message);
+            return message;            
+        }
+        #endregion
+
+        #region IDisposable
+        public void Dispose()
+        {
+            Console.WriteLine("BaseService.Dispose()");
+        }
+        #endregion
+        ~BaseService()
+        {
+            Console.WriteLine("~BaseService()");
+        }
+    }
+
+    public class FacadeService : IService
+    {
+        private readonly IService m_service;
+
+        public FacadeService(IService service)
+        {
+            m_service = service;
+            Console.WriteLine("Created FacadeService");
+        }
+        #region IService
+        public string Foo(string message)
+        {
+            Console.WriteLine("    FacadeService({0}", message);
+            return m_service.Foo(message);
+        }
+        #endregion
     }
 }
