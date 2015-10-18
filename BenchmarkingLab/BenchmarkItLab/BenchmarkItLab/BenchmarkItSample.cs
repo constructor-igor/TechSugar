46be967d233d817e9f151a8ea80bcf2e3a43f8fa
constructor-igor <igor.zc@gmail.com>
2015-10-18 09:58:54 +0300

    added BenchmarkItSample

---
+++ BenchmarkingLab/BenchmarkItLab/BenchmarkItLab/BenchmarkItSample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BenchmarkIt;
+using NUnit.Framework;
+
+namespace BenchmarkItLab
+{
+    [TestFixture]
+    public class BenchmarkItSample
+    {
+        [Test]
+        public void Sample()
+        {
+            Benchmark
+                .This("string.Contains", () => "abcdef".Contains("ef"))
+            .Against
+                .This("string.IndexOf", () => "abcdef".IndexOf("ef"))
+                .WithWarmup(1)
+                .For(5)
+                .Seconds().PrintComparison();
+        }
+    }
+}
