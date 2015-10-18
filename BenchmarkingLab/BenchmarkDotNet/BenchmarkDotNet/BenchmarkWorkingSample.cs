2eb3855a9ae070ede931fd18cd10b082dc976267
constructor-igor <igor.zc@gmail.com>
2015-10-18 09:29:57 +0300

    created BenchmarkDotNetLab

---
+++ BenchmarkingLab/BenchmarkDotNet/BenchmarkDotNet/BenchmarkWorkingSample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BenchmarkDotNet;
+using NUnit.Framework;
+
+namespace BenchmarkDotNetLab
+{
+    [TestFixture]
+    public class BenchmarkWorkingSample
+    {
+        private const string TEXT = "the test shows benchmark samples";
+        private const string TERMIN = "benchmark";
+        [Test]
+        public void RunBenchmark()
+        {
+            new BenchmarkRunner().RunCompetition(new BenchmarkWorkingSample());
+        }
+
+        [Benchmark]
+        [Test, Explicit]
+        public void IndexOfSample()
+        {
+            bool contains = TEXT.IndexOf(TERMIN, StringComparison.Ordinal) >= 0;
+            Assert.That(contains, Is.True);
+        }
+        [Benchmark]
+        [Test, Explicit]
+        public void ContainsSample()
+        {
+            bool contains = TEXT.Contains(TERMIN);
+            Assert.That(contains, Is.True);
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            bool contains1 = TEXT.IndexOf(TERMIN, StringComparison.Ordinal) >= 0;
+            Assert.That(contains1, Is.True);
+
+            bool contains2 = TEXT.Contains(TERMIN);
+            Assert.That(contains2, Is.True);
+        }
+    }
+}
