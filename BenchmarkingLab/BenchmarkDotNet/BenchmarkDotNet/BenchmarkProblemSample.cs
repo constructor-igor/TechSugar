e5c770408fdd71dea384c33c2da31bb857f0bdf5
constructor-igor <igor.zc@gmail.com>
2015-10-18 09:39:18 +0300

    added BenchmarkProblemSample

--- BenchmarkingLab/BenchmarkDotNet/BenchmarkDotNet/BenchmarkProblemSample.cs
+++ BenchmarkingLab/BenchmarkDotNet/BenchmarkDotNet/BenchmarkProblemSample.cs
@@ -1,39 +1,36 @@
-﻿using System;
 using BenchmarkDotNet;
+using BusinessLogic;
 using NUnit.Framework;
 
 namespace BenchmarkDotNetLab
 {
     [TestFixture]
-    public class BenchmarkWorkingSample
+    public class BenchmarkProblemSample
     {
         private const string TEXT = "the test shows benchmark samples";
         private const string TERMIN = "benchmark";
         [Test]
         public void RunBenchmark()
         {
-            new BenchmarkRunner().RunCompetition(new BenchmarkWorkingSample());
+            new BenchmarkRunner().RunCompetition(new BenchmarkProblemSample());
         }
 
         [Benchmark]
         public void IndexOfSample()
         {
-            bool contains = TEXT.IndexOf(TERMIN, StringComparison.Ordinal) >= 0;
+            BusinessLogicImpl.IndexOfSample();
         }
         [Benchmark]
         public void ContainsSample()
         {
-            bool contains = TEXT.Contains(TERMIN);
+            BusinessLogicImpl.ContainsSample();
         }
 
         [Test]
         public void SimpleTest()
         {
-            bool contains1 = TEXT.IndexOf(TERMIN, StringComparison.Ordinal) >= 0;
-            Assert.That(contains1, Is.True);
-
-            bool contains2 = TEXT.Contains(TERMIN);
-            Assert.That(contains2, Is.True);
+            BusinessLogicImpl.IndexOfSample();
+            BusinessLogicImpl.ContainsSample();
         }
     }
 }
---
+++ BenchmarkingLab/BenchmarkDotNet/BusinessLogic/BusinessLogicImpl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public class BusinessLogicImpl
+    {
+        private const string TEXT = "the test shows benchmark samples";
+        private const string TERMIN = "benchmark";
+
+        public static void IndexOfSample()
+        {
+            bool contains = TEXT.IndexOf(TERMIN, StringComparison.Ordinal) >= 0;
+        }
+        public static void ContainsSample()
+        {
+            bool contains = TEXT.Contains(TERMIN);
+        }
+    }
+}
