d135dfbb8a520680b69441684fc393c901d6b54e
constructor-igor <igor.zc@gmail.com>
2015-11-04 12:46:52 +0200

    added test StringBuilderCapacity

--- BenchmarkingLab/BenchmarkItLab/BenchmarkItLab/BenchmarkItSample.cs
+++ BenchmarkingLab/BenchmarkItLab/BenchmarkItLab/BenchmarkItSample.cs
@@ -1,4 +1,6 @@
-﻿using BenchmarkIt;
+﻿using System.Linq;
+using System.Text;
+using BenchmarkIt;
 using NUnit.Framework;
 
 namespace BenchmarkItLab
@@ -7,15 +9,49 @@ namespace BenchmarkItLab
     public class BenchmarkItSample
     {
         [Test]
-        public void Sample()
+        public void String_Contains_vs_IndexOf()
         {
             Benchmark
                 .This("string.Contains", () => "abcdef".Contains("ef"))
             .Against
                 .This("string.IndexOf", () => "abcdef".IndexOf("ef"))
-                .WithWarmup(1)
-                .For(5)
-                .Seconds().PrintComparison();
+            .WithWarmup(1)
+            .For(5)
+            .Seconds().PrintComparison();
+        }
+
+        [Test]
+        public void StringBuilderCapacity()
+        {
+            var list = Enumerable.Range(1, 256).ToList();
+            Benchmark
+                    .This("capacity 1", () =>
+                    {
+                        StringBuilder sb = new StringBuilder(1);
+                        list.ForEach(index => sb.Append("0"));
+                    })
+            .Against
+                    .This("capacity default(16)", () =>
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        list.ForEach(index => sb.Append("0"));
+                    })
+            .Against
+                    .This("capacity 256", () =>
+                    {
+                        StringBuilder sb = new StringBuilder(256);
+                        list.ForEach(index => sb.Append("0"));
+                    })
+            .Against
+                    .This("capacity 1024", () =>
+                    {
+                        StringBuilder sb = new StringBuilder(1024);
+                        list.ForEach(index => sb.Append("0"));
+                    })
+            .WithWarmup(2)
+            .For(100000)
+            .Iterations()            
+            .PrintComparison();
         }
     }
 }
