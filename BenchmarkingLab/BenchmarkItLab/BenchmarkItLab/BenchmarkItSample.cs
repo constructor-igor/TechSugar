4cafa6430a044e01f4f0d1d031dd14f8852dbf3d
constructor-igor <igor.zc@gmail.com>
2015-11-04 12:59:13 +0200

    added test ToArrayToList_vs_IEnumerable

--- BenchmarkingLab/BenchmarkItLab/BenchmarkItLab/BenchmarkItSample.cs
+++ BenchmarkingLab/BenchmarkItLab/BenchmarkItLab/BenchmarkItSample.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using BenchmarkIt;
 using NUnit.Framework;
@@ -52,6 +53,41 @@ namespace BenchmarkItLab
             .For(100000)
             .Iterations()            
             .PrintComparison();
+        }
+        [Test]
+        public void ToArrayToList_vs_IEnumerable()
+        {
+            IEnumerable<int> list = Create();
+            int count = 1024*1024;
+            Benchmark
+                    .This("IEnumerable", () =>
+                    {
+                        count = Foo(list);
+                    })
+            .Against
+                    .This("ToArray", () =>
+                    {
+                        count = Foo(list.ToArray());
+                    })
+            .Against
+                    .This("ToList", () =>
+                    {
+                        count = Foo(list.ToList());
+                    })
+            .WithWarmup(2)
+            .For(5)
+            .Seconds()
+            .PrintComparison();
+            Assert.That(count, Is.EqualTo(1024*1024));
+        }
+
+        IEnumerable<int> Create()
+        {
+            return Enumerable.Range(1, 1024*1024);
+        }
+        int Foo(IEnumerable<int> list)
+        {
+            return list.Count();
         }
     }
 }
