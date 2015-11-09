3a303b0338db8a9f1f64101b610246a1c45b4313
constructor-igor <igor.zc@gmail.com>
2015-11-09 17:23:23 +0200

    added NUnitIssue924

---
+++ TestingLab/NUnitSamples/NUnit_v3_samples/NUnitIssue924.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnit_v3_samples
+{
+    [TestFixture]
+    public class NUnitIssue924
+    {
+        [Test]
+        public async void AsyncError()
+        {
+            CancellationTokenSource source = new CancellationTokenSource();
+            var token = source.Token;
+            int i = 0;
+
+            var t = Task.Factory.StartNew(() =>
+            {
+
+                token.Register(() => { Debug.WriteLine("End"); }, true);
+
+                while (i++ < 20 && !token.IsCancellationRequested)
+                {
+                    Debug.WriteLine(i.ToString());
+                    Thread.Sleep(100);
+                }
+            }, token, TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
+
+            var t1 = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(500);
+                source.Cancel();
+            });
+
+            try
+            {
+                Task.WaitAll(t, t1);
+            }
+            finally
+            {
+                source.Dispose();
+            }
+        }
+    }
+}
--- TestingLab/NUnitSamples/NUnit_v3_samples/runTest.cmd
+++ TestingLab/NUnitSamples/NUnit_v3_samples/runTest.cmd
@@ -1,5 +1,5 @@
 ﻿rem
 rem
 rem
-D:\My\MyProjects\@NUnit\nunit_v3_beta4\NUnit-3.0.0-beta-4\bin\nunit-console.exe bin\debug\NUnit_v3_samples.dll --test=NUnit_v3_samples.NUnitIssue52
+D:\My\MyProjects\@NUnit\nunit_v3_rc2\NUnit-3.0.0-rc-2\bin\nunit3-console.exe bin\debug\NUnit_v3_samples.dll --test=NUnit_v3_samples.NUnitIssue924
 pause
