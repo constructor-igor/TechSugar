0d783bba200eddc8cdde931b1801bf9c94280ccc
constructor-igor <igor.zc@gmail.com>
2015-11-11 09:38:15 +0200

    added ExpectedExceptionTests

---
+++ TestingLab/NUnitSamples/NUnit_v3_samples/ExpectedExceptionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit_v3_samples.Infra;
+
+namespace NUnit_v3_samples
+{
+    //
+    // https://github.com/nunit/nunit-csharp-samples/blob/master/ExpectedExceptionExample/ExpectedExceptionAttribute.cs
+    //
+    [TestFixture]
+    public class ExpectedExceptionTests
+    {
+        [Test]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void InvalidArgument_ExpectedException_DivideByZeroException()
+        {
+            double r = Div(2.0, 0.0);
+        }
+
+        double Div(double x, double y)
+        {
+            if (y == 0)
+                throw new DivideByZeroException();
+            return x/y;
+        }
+    }
+}
