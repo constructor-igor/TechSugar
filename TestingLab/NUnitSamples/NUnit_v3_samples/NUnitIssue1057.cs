f2950e19f8550b6fec2aa5e15aca654277ffb0b1
constructor-igor <igor.zc@gmail.com>
2015-12-07 09:48:54 +0200

    added nunit issue 1057

---
+++ TestingLab/NUnitSamples/NUnit_v3_samples/NUnitIssue1057.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnit_v3_samples
+{
+    [TestFixture]
+    public class NUnitIssue1057
+    {
+        [Test]
+        public void OriginalTest()
+        {
+            var address = "https://gist.githubusercontent.com/lbergnehr/56c355d41454855c25a4/raw/f4b95565a2330b695021c3d4b4fb55ff40aa4923/nunit_string_contains_test";
+            var result = new System.Net.WebClient().DownloadString(address);
+            Assert.That(result, Does.Contain(Guid.NewGuid().ToString()));
+        }
+        [Test]
+        public void Test()
+        {
+            //var address = "https://gist.githubusercontent.com/lbergnehr/56c355d41454855c25a4/raw/f4b95565a2330b695021c3d4b4fb55ff40aa4923/nunit_string_contains_test";
+            string result = Guid.NewGuid().ToString();
+            Assert.That(result, Does.Contain(Guid.NewGuid().ToString()));
+        }
+    }
+}
--- TestingLab/NUnitSamples/NUnit_v3_samples/runTest_v3.cmd
+++ TestingLab/NUnitSamples/NUnit_v3_samples/runTest_v3.cmd
@@ -1,6 +1,6 @@
 ﻿rem
 rem
 rem
-D:\My\MyProjects\@NUnit\NUnit-3.0.1\bin\nunit3-console.exe bin\debug\NUnit_v3_samples.dll --test=NUnit_v3_samples.NUnitIssue1111
+D:\My\MyProjects\@NUnit\NUnit-3.0.1\bin\nunit3-console.exe bin\debug\NUnit_v3_samples.dll --test=NUnit_v3_samples.NUnitIssue1057
 rem D:\My\MyProjects\@NUnit\NUnit-3.0.0\bin\nunit3-console.exe bin\debug\NUnit_v3_samples.dll --test=NUnit_v3_samples.NUnitIssue1033
 pause
