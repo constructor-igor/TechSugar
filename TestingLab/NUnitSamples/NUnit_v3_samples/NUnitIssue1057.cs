ac9575509c047405bbbb692eb7084a2abe918e26
constructor-igor <igor.zc@gmail.com>
2015-12-07 09:49:28 +0200

    updated test 1057

--- TestingLab/NUnitSamples/NUnit_v3_samples/NUnitIssue1057.cs
+++ TestingLab/NUnitSamples/NUnit_v3_samples/NUnitIssue1057.cs
@@ -9,9 +9,9 @@ namespace NUnit_v3_samples
         [Test]
         public void OriginalTest()
         {
-            var address = "https://gist.githubusercontent.com/lbergnehr/56c355d41454855c25a4/raw/f4b95565a2330b695021c3d4b4fb55ff40aa4923/nunit_string_contains_test";
-            var result = new System.Net.WebClient().DownloadString(address);
-            Assert.That(result, Does.Contain(Guid.NewGuid().ToString()));
+            //var address = "https://gist.githubusercontent.com/lbergnehr/56c355d41454855c25a4/raw/f4b95565a2330b695021c3d4b4fb55ff40aa4923/nunit_string_contains_test";
+            //var result = new System.Net.WebClient().DownloadString(address);
+            //Assert.That(result, Does.Contain(Guid.NewGuid().ToString()));
         }
         [Test]
         public void Test()
