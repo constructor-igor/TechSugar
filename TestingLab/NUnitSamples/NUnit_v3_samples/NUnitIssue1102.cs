563e8eb73427ddd7613e8caa851d521ffdd3c5a4
constructor-igor <igor.zc@gmail.com>
2015-12-14 11:13:30 +0200

    added NUnit issue 1102

---
+++ TestingLab/NUnitSamples/NUnit_v3_samples/NUnitIssue1102.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace NUnit_v3_samples
+{
+    [TestFixture]
+    public class NUnitIssue1102
+    {
+        [OneTimeSetUp]
+        public virtual void RunBeforeAnyTests()
+        {
+            string testName = TestContext.CurrentContext.Test.FullName;
+            Console.WriteLine("{0}: RunBeforeAnyTests ({1})", Thread.CurrentThread.ManagedThreadId, testName);            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            string testName = TestContext.CurrentContext.Test.FullName;
+            Console.WriteLine("{0}: RunBeforeAnyTests ({1})", Thread.CurrentThread.ManagedThreadId, testName);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string testName = TestContext.CurrentContext.Test.FullName;
+            Console.WriteLine("{0}: RunBeforeAnyTests ({1})", Thread.CurrentThread.ManagedThreadId, testName);
+        }
+    }
+}
