684dbb29facaf29b9af5ad3d401f5e2a17a0fffe
constructor-igor <igor.zc@gmail.com>
2016-02-02 12:21:13 +0200

    added Program.cs

--- GpuByCSharp/Gpu-Cudafy-Samples/Program.cs
+++ GpuByCSharp/Gpu-Cudafy-Samples/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Gpu_Cudafy_Samples
+﻿using System;
+using Cudafy;
+using Cudafy.Translator;
+
+/*
+ * 
+ * original samples copied from https://cudafy.codeplex.com/
+ * 
+ * */  
+
+namespace Gpu_Cudafy_Samples
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int deviceId = 0;
+            CudafyModes.Target = eGPUType.OpenCL;
+            //CudafyModes.Target = eGPUType.Cuda;
+            CudafyModes.DeviceId = deviceId;
+            CudafyTranslator.Language = CudafyModes.Target == eGPUType.OpenCL ? eLanguage.OpenCL : eLanguage.Cuda;
+
+            Console.WriteLine("\r\nArrayBasicIndexing");
+            ArrayBasicIndexing.Execute(deviceId);
+
+            Console.ReadLine();
         }
     }
 }
