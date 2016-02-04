4029df2f83fc6eb5891d6c9274d12c651ffde53d
constructor-igor <igor.zc@gmail.com>
2016-02-04 23:12:30 +0200

    #60: implemented simple test for NUnitForms

---
+++ WinForm/TaskInUI/NunitFormsTests/MyUITests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Extensions.Forms;
+using NUnit.Framework;
+using TaskInUI;
+
+namespace NunitFormsTests
+{
+    //
+    // https://github.com/constructor-igor/TechSugar/issues/60
+    //
+    [TestFixture]
+    public class MyUITests//: NUnitFormTest
+    {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.Pass("dummy test");
+        }
+        [Test]
+        public void Test()
+        {
+            MainForm testedForm = new MainForm();
+            testedForm.Show();
+
+            ButtonTester buttonTester = new ButtonTester("button1", "MainForm");
+            Assert.That(buttonTester.Text, Is.EqualTo("Start \"Worker\""));
+            buttonTester.Click();
+        }
+    }
+}
