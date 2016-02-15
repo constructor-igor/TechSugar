2ea4daa0390061ac5a11e93a498c266d6c8ec7bb
constructor-igor <igor.zc@gmail.com>
2016-02-15 12:59:11 +0200

    added test NUnitIssue1266 for https://github.com/nunit/nunit/issues/1266

---
+++ TestingLab/NUnitSamples/NUnit_v3_samples/NUnitIssue1266.cs
@@ -0,0 +1,24 @@
+using System.Globalization;
+using NUnit.Framework;
+[assembly: SetCulture("")]
+
+namespace NUnit_v3_samples
+{    
+    [TestFixture]
+    //[SetCulture("")]
+    public class NUnitIssue1266
+    {
+        [Test]
+        //[SetCulture("")]
+        public void It_should_set_proper_culture()
+        {
+            Assert.That(CultureInfo.CurrentCulture, Is.EqualTo(CultureInfo.InvariantCulture));
+        }
+
+//        [Test]
+//        public void It_should_not_set_proper_culture()
+//        {
+//            Assert.That(CultureInfo.CurrentCulture, Is.Not.EqualTo(CultureInfo.InvariantCulture));
+//        }
+    }
+}
--- TestingLab/NUnitSamples/NUnit_v3_samples/runTest_v3.cmd
+++ TestingLab/NUnitSamples/NUnit_v3_samples/runTest_v3.cmd
@@ -1,5 +1,5 @@
 ﻿rem
 rem
 rem
-D:\My\MyProjects\@NUnit\NUnit-3.0.1\bin\nunit3-console.exe bin\debug\NUnit_v3_samples.dll --test=NUnit_v3_samples.NUnitIssue1109
+D:\My\MyProjects\@NUnit\NUnit-3.0.1\bin\nunit3-console.exe bin\debug\NUnit_v3_samples.dll --test=NUnit_v3_samples.NUnitIssue1266
 pause
