a6a1fce8a5b4f45869138637c70a86372f61164f
constructor-igor <igor.zc@gmail.com>
2016-02-17 15:27:49 +0200

    #68 refactoring

---
+++ csharp-tips/csharp-tips/csharp-tips/DynamicProxySamples/EmitSamples.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace csharp_tips.DynamicProxySamples
+{
+    [TestFixture]
+    public class EmitSamples
+    {
+        /*
+         * References:
+         * - http://www.codeproject.com/Articles/5511/Dynamic-Proxy-Creation-Using-C-Emit
+         * 
+         * */
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
--- csharp-tips/csharp-tips/csharp-tips/DynamicProxySamples/IDoStuff.cs
+++ csharp-tips/csharp-tips/csharp-tips/DynamicProxySamples/IDoStuff.cs
@@ -1,7 +1,20 @@
+using System;
+
 namespace csharp_tips.DynamicProxySamples
 {
     public interface IDoStuff
     {
         void Foo();
     }
+
+    public class ActualDoStuff : MarshalByRefObject, IDoStuff
+    {
+        #region IDoStuff
+        public void Foo()
+        {
+            Console.WriteLine("Actual Foo");
+        }
+        #endregion
+    }
+
 }
