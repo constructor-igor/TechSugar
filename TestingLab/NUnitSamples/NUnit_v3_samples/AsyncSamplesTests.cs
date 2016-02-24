b5f1ab40ea1ae2a169d14c0e3315f637bac8b01e
constructor-igor <igor.zc@gmail.com>
2016-02-24 10:11:29 +0200

    #76 test

---
+++ TestingLab/NUnitSamples/NUnit_v3_samples/AsyncSamplesTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnit_v3_samples
+{
+    [TestFixture]
+    public class AsyncSamplesTests
+    {
+        [Test]
+        public void OneSimpleSyncTest()
+        {
+            var eightBall = new EightBall();
+            var answer = eightBall.ShouldIChangeJobSync();
+
+            Assert.That(answer, Is.True );
+        }
+
+        [TestCase(ExpectedResult=true)]
+        public async Task<bool> OneSimpleASyncTest()
+        {
+            var eightBall = new EightBall();
+            return await eightBall.ShouldIChangeJobASync();
+            //return answer;
+            //answer.Wait();
+
+            //Assert.That(answer, Is.True);
+
+            // why am I still here?
+        }
+
+        [Test]
+        public async Task EqualToTest()
+        {
+            Assert.That(5, Is.EqualTo(6));
+        }
+    }
+
+    public class EightBall
+    {
+        public bool ShouldIChangeJobSync()
+        {
+            return true;
+        }
+        public Task<bool> ShouldIChangeJobASync()
+        {
+            Task<bool> workingTask = new Task<bool>(() => true);
+            workingTask.Start();
+            return workingTask;            
+        }
+    }
+}
