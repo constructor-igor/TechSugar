a1e510a83a7f6e06872cba93a7ae9575edad4206
constructor-igor <igor.zc@gmail.com>
2016-02-28 17:23:37 +0200

    #1 minor

---
+++ API/Amazon/AmazonSOAP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Amazon.PAAPI
+{
+    /*
+     * downloaded from
+     * */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Amazon ProductAdvertisingAPI client
+            AWSECommerceServicePortTypeClient amazonClient = new AWSECommerceServicePortTypeClient();
+
+            // prepare an ItemSearch request
+            ItemSearchRequest request = new ItemSearchRequest
+            {
+                SearchIndex = "Books",
+                Title = "WCF",
+                ResponseGroup = new[] {"Small"}
+            };
+
+            ItemSearch itemSearch = new ItemSearch
+            {
+                Request = new[] {request},
+                AWSAccessKeyId = ConfigurationManager.AppSettings["accessKeyId"],
+                AssociateTag = "ReplaceWithYourValue"
+            };
+
+            // send the ItemSearch request
+            ItemSearchResponse response = amazonClient.ItemSearch(itemSearch);
+
+            // write out the results from the ItemSearch request
+            foreach (var item in response.Items[0].Item)
+            {
+                Console.WriteLine(item.ItemAttributes.Title);
+            }
+            Console.WriteLine("done...enter any key to continue>");
+            Console.ReadLine();
+        }
+    }
+}
