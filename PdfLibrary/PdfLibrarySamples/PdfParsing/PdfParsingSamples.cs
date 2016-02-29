150bb72e348138d24583719ead96c4a629a1c8e4
constructor-igor <igor.zc@gmail.com>
2016-02-29 15:32:06 +0200

    #78 simple test of .pdf text extracting

---
+++ PdfLibrary/PdfLibrarySamples/PdfParsing/PdfParsingSamples.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using NUnit.Framework;
+
+namespace PdfParsing
+{
+    /*
+     * References:
+     * - http://stackoverflow.com/questions/83152/reading-pdf-documents-in-net
+     * 
+     * */
+    [TestFixture]
+    public class PdfParsingSamples
+    {
+        [Test]
+        public void ParsePdfSample()
+        {
+            string pdfFilePath = @"..\..\data\Spec.pdf";
+            Assert.That(File.Exists(pdfFilePath), Is.True);
+
+            using (PdfReader reader = new PdfReader(pdfFilePath))
+            {
+                Assert.That(reader.NumberOfPages, Is.EqualTo(1));
+
+                string text = "";
+                for (int page = 1; page <= reader.NumberOfPages; page++)
+                {
+                    text += PdfTextExtractor.GetTextFromPage(reader, page);
+                }
+
+                Assert.That(text, Is.StringStarting("Console application"));
+            }
+        }
+    }
+}
