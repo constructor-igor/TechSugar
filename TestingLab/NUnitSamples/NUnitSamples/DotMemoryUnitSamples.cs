a47cb41d82b79cd826692656ade97042e7d427ae
constructor-igor <igor.zc@gmail.com>
2016-03-20 18:11:18 +0200

    #5 updated DotMemoryUnitSamples

--- TestingLab/NUnitSamples/NUnitSamples/DotMemoryUnitSamples.cs
+++ TestingLab/NUnitSamples/NUnitSamples/DotMemoryUnitSamples.cs
@@ -5,11 +5,18 @@ namespace NUnit_v2_samples
 {
     public interface IFoo
     {
-        
+        void Run();
     }
     public class Foo : IFoo
     {
-        
+        private byte[] m_data;
+        #region Implementation of IFoo
+        public void Run()
+        {
+            m_data = new byte[1024];
+        }
+
+        #endregion
     }
 
     [TestFixture]
@@ -30,6 +37,31 @@ namespace NUnit_v2_samples
             dotMemory.Check(memory =>
             {
                 Assert.That(memory.GetObjects(where => where.Type.Is<Foo>()).ObjectsCount, Is.EqualTo(1));
+            });
+        }
+
+        [AssertTraffic(AllocatedSizeInBytes = 1000)]
+        [Test]
+        public void MemoryAllocationTest()
+        {
+            //byte[] data = new byte[1024];
+        }
+
+        [DotMemoryUnit(CollectAllocations = true)]
+        [Test]
+        public void MemoryAllocationTrendTest()
+        {
+            IFoo foo = new Foo();
+            var memoryCheckpoint1 = dotMemory.Check();
+
+            foo.Run();
+
+            var memoryCheckPoint2 = dotMemory.Check(memory =>
+            {
+                Assert.That(
+                    memory.GetTrafficFrom(memoryCheckpoint1)
+                        .Where(obj => obj.Interface.Is<IFoo>())
+                        .AllocatedMemory.SizeInBytes, Is.LessThan(1000));
             });
         }
     }
