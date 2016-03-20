d314c8735ad6cea1af50c88782044f3be67f7f3e
constructor-igor <igor.zc@gmail.com>
2016-03-20 14:48:14 +0200

    #68 updated "dynamic proxy"

---
+++ ArchitectsLab/DynamicProxy/IProxy.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace DynamicProxy
+{
+    public enum Direction { In, Out }
+
+    public partial interface IProxy<T>
+    {
+        /// <summary>
+        /// Main functionOrProperty for adding transformers.
+        /// </summary>
+        /// <typeparam name="T1">Input Type</typeparam>
+        /// <typeparam name="TResult">Result Type</typeparam>
+        /// <param name="functionOrProperty">An expression pointing to the fucntion or a function or parameter</param>
+        /// <param name="direction">Whether you want to transform the input or the output <see cref="Direction"/></param>
+        /// <param name="transformer">The delegate containing the transformer.</param>
+        /// <returns>The IProxy so you can chain adding Transformers and Interceptors.</returns>
+        IProxy<T> AddTransformer<T1, TResult>(Expression functionOrProperty, Direction direction, Func<T1, TResult> transformer);
+
+        /// <summary>
+        /// Convenience function for adding a transformer with the same input as output to a function.
+        /// </summary>
+        /// <typeparam name="T1">The in and output type.</typeparam>
+        /// <param name="function">An expression pointing to a function.</param>
+        /// <param name="direction">Whether you want to transform the result or a parameter.</param>
+        /// <param name="transformer">The delegate containing the transformer.</param>
+        /// <returns>The IProxy so you can chain adding Transformers and Interceptors.</returns>
+        IProxy<T> AddTransformer<T1>(Expression<Action<T>> function, Direction direction, Func<T1, T1> transformer);
+
+        /// <summary>
+        /// Function to add a transformer to a function.
+        /// </summary>
+        /// <typeparam name="T1">The in and output type.</typeparam>
+        /// <typeparam name="TResult">The resulting type</typeparam>
+        /// <param name="function">An expression pointing to a function.</param>
+        /// <param name="direction">Whether you want to transform the result or a parameter.</param>
+        /// <param name="transformer">The delegate containing the transformer.</param>
+        /// <returns>The IProxy so you can chain adding Transformers and Interceptors.</returns>
+        IProxy<T> AddTransformer<T1, TResult>(Expression<Action<T>> function, Direction direction, Func<T1, TResult> transformer);
+
+        /// <summary>
+        /// Function to add a transformer to a function
+        /// </summary>
+        /// <typeparam name="T1">The in and output type.</typeparam>
+        /// <typeparam name="TResult">The resulting type.</typeparam>
+        /// <param name="property">An expression pointing to a function.</param>
+        /// <param name="direction">Whether you want to transform the result or a parameter.</param>
+        /// <param name="transformer">The delegate containing the transformer.</param>
+        /// <returns>The IProxy so you can chain adding Transformers and Interceptors.</returns>
+        IProxy<T> AddTransformer<T1, TResult>(Expression<Func<T, T1>> property, Direction direction, Func<T1, TResult> transformer);
+        
+        /// <summary>
+        /// Add interceptor to void function without parameters;
+        /// </summary>
+        /// <param name="function">An expression pointing to a function</param>
+        /// <param name="interceptor">The delegate containing the interceptor.</param>
+        /// <returns>The IProxy so you can chain adding Transformers and Interceptors.</returns>
+        IProxy<T> AddInterceptor(Expression<Action<T>> function, Action<Action> interceptor);
+
+        /// <summary>
+        /// Add Interceptor to PropertyGet that doesn't change the type
+        /// </summary>
+        /// <typeparam name="TProp">Type of the property</typeparam>
+        /// <param name="property">Expression pointing to the function</param>
+        /// <param name="interceptor">The Interceptor delegate</param>
+        /// <returns>The IProxy so you can chain adding Transformers and Interceptors.</returns>
+        IProxy<T> AddInterceptor<TProp>(Expression<Func<T, TProp>> property, Func<Func<TProp>, TProp> interceptor);
+
+        /// <summary>
+        /// Add Interceptor to PropertyGet
+        /// </summary>
+        /// <typeparam name="TOriginal">Type of the property</typeparam>
+        /// <typeparam name="TNew">The new type of the property</typeparam>
+        /// <param name="property">Expression pointing to the function</param>
+        /// <param name="interceptor">The Interceptor delegate</param>
+        /// <returns>The IProxy so you can chain adding Transformers and Interceptors.</returns>
+        IProxy<T> AddInterceptor<TOriginal, TNew>(Expression<Func<T, TOriginal>> property, Func<Func<TOriginal>, TNew> interceptor);
+
+        /// <summary>
+        /// Add Interceptor to PropertySet
+        /// </summary>
+        /// <typeparam name="TProp">Type of the property</typeparam>
+        /// <param name="property">Expression pointing to the function</param>
+        /// <param name="interceptor">The Interceptor delegate</param>
+        /// <returns>The IProxy so you can chain adding Transformers and Interceptors.</returns>
+        IProxy<T> AddInterceptor<TProp>(Expression<Func<T, TProp>> property, Action<Action<TProp>, TProp> interceptor);
+
+        /// <summary>
+        /// Add Interceptor to PropertySet
+        /// </summary>
+        /// <typeparam name="TOriginal">Type of the property</typeparam>
+        /// <typeparam name="TNew">The new type of the property</typeparam>
+        /// <param name="property">Expression pointing to the function</param>
+        /// <param name="interceptor">The Interceptor delegate</param>
+        /// <returns>The IProxy so you can chain adding Transformers and Interceptors.</returns>
+        IProxy<T> AddInterceptor<TOriginal, TNew>(Expression<Func<T, TOriginal>> property, Action<Action<TOriginal>, TNew> interceptor);
+
+        //IProxy<T> AddFunctionInterceptor(Expression<Action<T>> function, Func<Func<object[], object>, object[], object> func);
+        //IProxy<T> AddPropertyInterceptor(Expression property, Func<Func<object[], object>, object[], object> func, Direction direction);
+    }
+}
--- ArchitectsLab/DynamicProxy/Properties/AssemblyInfo.cs
+++ ArchitectsLab/DynamicProxy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AmazonConsole")]
+[assembly: AssemblyTitle("DynamicProxy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AmazonConsole")]
+[assembly: AssemblyProduct("DynamicProxy")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f73d4dee-dc3d-49e7-b959-c1fdd7628ca3")]
+[assembly: Guid("fd236c59-3f01-4bc9-a99d-be008ddf7bb3")]
 
 // Version information for an assembly consists of the following four values:
 //
