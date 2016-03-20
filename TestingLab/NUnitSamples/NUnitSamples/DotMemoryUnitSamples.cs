f65905b1a55cf55ff3a4b0cea936350cf9742766
constructor-igor <igor.zc@gmail.com>
2016-03-20 15:36:05 +0200

    #5 created "dummy" test

---
+++ TestingLab/NUnitSamples/NUnitSamples/DotMemoryUnitSamples.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NUnit_v2_samples
+{
+    [TestFixture]
+    public class DotMemoryUnitSamples
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
