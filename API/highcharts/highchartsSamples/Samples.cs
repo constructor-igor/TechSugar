1d77b1ad98697cbf59acd92c1d775bc4e4776ef1
constructor-igor <igor.zc@gmail.com>
2016-03-20 14:03:33 +0200

    #114 created sample (nuget package cannot be installed)

---
+++ API/highcharts/highchartsSamples/Samples.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using NUnit.Framework;
+
+namespace highchartsSamples
+{
+    /*
+     * References:
+     * - https://habrahabr.ru/post/279515/
+     * 
+     * */
+    [TestFixture]
+    public class Samples
+    {
+        [Test]
+        public async void PostSample()
+        {
+            throw new NotImplementedException();
+//            var client = new HighchartsClient("http://export.highcharts.com/");
+//
+//            var options = new
+//            {
+//                xAxis = new
+//                {
+//                    categories = new[] { "Jan", "Feb", "Mar" }
+//                },
+//                series = new[]
+//                {
+//                    new { data = new[] {29.9, 71.5, 106.4} }
+//                }
+//            };
+//
+//            var res = await client.GetChartImageFromOptionsAsync(JsonConvert.SerializeObject(options));
+//
+//            File.WriteAllBytes("image.png", res);
+        }
+    }
+}
