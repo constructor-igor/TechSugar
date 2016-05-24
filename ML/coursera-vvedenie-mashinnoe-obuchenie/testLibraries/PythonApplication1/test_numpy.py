8f3a06bd807dd09da5cdbdb06b3d90e771351b1f
constructor-igor <igor.zc@gmail.com>
2016-05-24 15:23:13 +0300

    #152 added samples

--- ML/coursera-vvedenie-mashinnoe-obuchenie/testLibraries/PythonApplication1/test_numpy.py
+++ ML/coursera-vvedenie-mashinnoe-obuchenie/testLibraries/PythonApplication1/test_numpy.py
@@ -1,5 +1,26 @@
 print('Hello World')
 import numpy
-numpy.test()
 
+X = np.random.normal(loc=1, scale=10, size=(1000, 50))
+X
 
+m = np.mean(X, axis=0)
+std = np.std(X, axis=0)
+X_norm = ((X - m)  / std)
+X_norm
+
+Z = np.array([[4, 5, 0], 
+             [1, 9, 3],              
+             [5, 1, 1],
+             [3, 3, 3], 
+             [9, 9, 9], 
+             [4, 7, 1]])
+r = np.sum(Z, axis=1)
+np.nonzero(r > 10)
+
+A = np.eye(3)
+B = np.eye(3)
+A
+B
+AB = np.vstack((A, B))
+AB
