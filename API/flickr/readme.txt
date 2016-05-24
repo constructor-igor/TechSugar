dc9f82d66d6d58e736f1ed8a5ca88a2b7229b3f4
constructor-igor <igor.zc@gmail.com>
2016-05-24 23:33:43 +0300

    Revert "git fetch"
    This reverts commit 896b0b6ad9f3cbbc5f4b7ebff28570104e7dd45c.

--- API/flickr/readme.txt
+++
@@ -1,2 +0,0 @@
-http://www.ezzylearning.com/tutorial/display-photos-in-asp-net-using-flickr-net-api
-https://github.com/samjudson/flickr-net
--- TestingLab/NUnitSamples/NUnitSamples/runTest.cmd
+++
@@ -1,7 +0,0 @@
-﻿rem
-rem
-rem
-rem "C:\Program Files (x86)\NUnit 2.6.4\bin\nunit-console.exe"
-rem "C:\Program Files (x86)\NUnit 2.6.4\bin\nunit-console.exe" bin\debug\NUnit_v2_samples.dll /fixture=NUnit_v2_samples.NUnitRepeatTests /result=result_v2.xml /output=output_v2.txt
-"C:\Program Files (x86)\NUnit 2.6.4\bin\nunit-console.exe" bin\debug\NUnit_v2_samples.dll /fixture=NUnit_v2_samples.NUnitIssue1109 /result=result_v2.xml /output=output_v2.txt
-pause
