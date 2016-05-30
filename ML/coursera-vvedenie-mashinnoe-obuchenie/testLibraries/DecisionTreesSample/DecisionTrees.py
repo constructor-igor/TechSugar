d6bc65f18ff01c10882f34c9ee224bdc07f50c5f
constructor-igor <igor.zc@gmail.com>
2016-05-30 11:43:49 +0300

    #152 updated Decision Tree sample

--- ML/coursera-vvedenie-mashinnoe-obuchenie/testLibraries/DecisionTreesSample/DecisionTrees.py
+++ ML/coursera-vvedenie-mashinnoe-obuchenie/testLibraries/DecisionTreesSample/DecisionTrees.py
@@ -4,3 +4,11 @@ X = np.array([[1, 2], [3, 4], [5, 6]])
 y = np.array([0, 1, 0])
 clf = DecisionTreeClassifier()
 clf.fit(X, y)
+
+importances = clf.feature_importances_
+
+testNanValue = float('nan')
+testOneValue = 1.0
+
+print np.isnan(testOneValue)
+print np.isnan(testNanValue)
