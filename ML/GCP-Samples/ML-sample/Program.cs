34d6557dab6a8e5dcc47db5222494f7ef3dc7adf
constructor-igor <igor.zc@gmail.com>
2016-06-05 17:47:15 +0300

    #156 created empty sample

---
+++ ML/GCP-Samples/ML-sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Http;
+using Google.Apis.Prediction.v1_6;
+using Google.Apis.Services;
+
+namespace ML_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//            new PredictionService();
+//            //var body = new Google.Apis.Prediction.v1_6.Data.Input();
+//            UserCredential credential = GoogleWebAuthorizationBroker.AuthorizeAsync()
+//
+//
+//            string API_KEY;
+//            IConfigurableHttpClientInitializer credential = new ComputeCredential();
+//            var service = new Google.Apis.Prediction.v1_6.PredictionService(new BaseClientService.Initializer
+//            {
+//                HttpClientInitializer = credential,
+//                ApplicationName = "Prediction Sample",
+//                ApiKey = API_KEY,
+//            });
+        }
+    }
+}
