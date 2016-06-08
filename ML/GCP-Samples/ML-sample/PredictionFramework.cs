a365f7df37e177c8d48f1fe489444277d70c7625
constructor-igor <igor.zc@gmail.com>
2016-06-08 09:45:42 +0300

    #156 refactoring: added commands "training" and "status"

---
+++ ML/GCP-Samples/ML-sample/PredictionFramework.cs
@@ -0,0 +1,67 @@
+using System;
+using Google.Apis.Prediction.v1_6;
+using Google.Apis.Prediction.v1_6.Data;
+using Google.Apis.Services;
+
+namespace ML_sample
+{
+    public class ProjectModelId
+    {
+        public readonly string PojectNumber;
+        public readonly string ModelId;
+
+        public ProjectModelId(string pojectNumber, string modelId)
+        {
+            PojectNumber = pojectNumber;
+            ModelId = modelId;
+        }
+    }
+    public class PredictionFramework
+    {
+        public PredictionService PredictionService; 
+        public void CreatePredictionService(string authJsonFile)
+        {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", authJsonFile);
+            var credentials = Google.Apis.Auth.OAuth2.GoogleCredential.GetApplicationDefaultAsync().Result;
+            if (credentials.IsCreateScopedRequired)
+            {
+                credentials = credentials.CreateScoped(PredictionService.Scope.DevstorageFullControl,
+                    PredictionService.Scope.Prediction);
+            }
+            var serviceInitializer = new BaseClientService.Initializer()
+            {
+                ApplicationName = "Prediction Sample",
+                HttpClientInitializer = credentials
+            };
+
+            PredictionService = new PredictionService(serviceInitializer);
+        }
+
+        public string DeleteTrainedModel(ProjectModelId projectModelId)
+        {
+            var deleteRequest = PredictionService.Trainedmodels.Delete(projectModelId.PojectNumber, projectModelId.ModelId);
+            string deleteResponse = deleteRequest.Execute();
+            return deleteResponse;
+        }
+
+        public Insert2 TrainRegressionModel(ProjectModelId projectModelId, string dataPath)
+        {
+            Insert insertBody = new Insert
+            {
+                StorageDataLocation = dataPath,
+                Id = projectModelId.ModelId,
+                ModelType = "REGRESSION"
+            };
+            TrainedmodelsResource.InsertRequest insertRequest = PredictionService.Trainedmodels.Insert(insertBody, projectModelId.PojectNumber);
+            Insert2 insertResponse = insertRequest.Execute();
+            return insertResponse;
+        }
+
+        public Insert2 GetModelStatus(ProjectModelId projectModelId)
+        {
+            var getRequest = PredictionService.Trainedmodels.Get(projectModelId.PojectNumber, projectModelId.ModelId);
+            Insert2 response = getRequest.Execute();
+            return response;
+        }
+    }
+}
--- ML/GCP-Samples/ML-sample/Program.cs
+++ ML/GCP-Samples/ML-sample/Program.cs
@@ -1,81 +1,80 @@
 ﻿using System;
 using System.Threading;
-using Google.Apis.Prediction.v1_6;
 using Google.Apis.Prediction.v1_6.Data;
-using Google.Apis.Services;
 
 namespace ML_sample
 {
     class Program
     {
-        const string PROJECT_ID = "har-machine-learning-1333";
+        const string AUTH_JSON_FILE = @"D:\@Temp\GCP_ML_Feasibility\HAR Machine Learning-41ae33f89eac.json";
+        const string MODEL_ID = "20000-test";
+        //const string PROJECT_ID = "har-machine-learning-1333";
         const string PROJECT_NUMBER = "1024476357063";
         const string STRORAGE_DATA = "good-test-case/testGood2.csv";
         const int PROGRESS_WAITING_TIME = 1000;
 
         static void Main(string[] args)
         {
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"D:\@Temp\GCP_ML_Feasibility\HAR Machine Learning-41ae33f89eac.json");
-            var credentials = Google.Apis.Auth.OAuth2.GoogleCredential.GetApplicationDefaultAsync().Result;
-            if (credentials.IsCreateScopedRequired)
+            PredictionFramework predictionFramework = new PredictionFramework();
+            predictionFramework.CreatePredictionService(AUTH_JSON_FILE);
+            ProjectModelId projectModelId = new ProjectModelId(PROJECT_NUMBER, MODEL_ID);
+            string command = "status";
+
+            Console.WriteLine("Project number: {0}, Model ID: {1}", projectModelId.PojectNumber, projectModelId.ModelId);
+            Console.WriteLine("Command: {0}", command);
+            
+            switch (command)
             {
-                credentials = credentials.CreateScoped(PredictionService.Scope.DevstorageFullControl, PredictionService.Scope.Prediction);
-            }
-            var serviceInitializer = new BaseClientService.Initializer()
-            {
-                ApplicationName = "Prediction Sample",
-                HttpClientInitializer = credentials
-            };
+                case "status":
+                    Insert2 trainingStatus = predictionFramework.GetModelStatus(projectModelId);
+                    Console.WriteLine("Training status: {0}", trainingStatus.TrainingStatus);
+                    Insert2.ModelInfoData stausModelInfoData = trainingStatus.ModelInfo;
+                    if (stausModelInfoData != null)
+                    {
+                        Console.WriteLine("ModelType: {0}", stausModelInfoData.ModelType);
+                        Console.WriteLine("NumberInstances: {0}", stausModelInfoData.NumberInstances);
+                        Console.WriteLine("MeanSquaredError: {0}", stausModelInfoData.MeanSquaredError);
+                    }
+                    break;
+                case "training":
+                    predictionFramework.DeleteTrainedModel(projectModelId);
+                    Insert2 insertResponse = predictionFramework.TrainRegressionModel(projectModelId, STRORAGE_DATA);
+                    Console.WriteLine("Inserted the training data for the model.");
+                    // Wait until the training is complete
+                    bool trainingRunning = true;
+                    while (trainingRunning)
+                    {
+                        Console.WriteLine("Getting a new training progress status...");
+                        var getResponse = predictionFramework.GetModelStatus(projectModelId);
+                        Console.WriteLine("Got a new training progress status: {0}", getResponse.TrainingStatus);
 
-            PredictionService predictionService = new PredictionService(serviceInitializer);
-
-            var deleteRequest = predictionService.Trainedmodels.Delete(PROJECT_NUMBER, PROJECT_ID);
-            string deleteResponse = deleteRequest.Execute();
-
-            Insert insertBody = new Insert
-            {
-                StorageDataLocation = STRORAGE_DATA, Id = PROJECT_ID, ModelType = "REGRESSION"
-            };
-            TrainedmodelsResource.InsertRequest insertRequest = predictionService.Trainedmodels.Insert(insertBody, PROJECT_NUMBER);
-            Insert2 insertResponse = insertRequest.Execute();            
-            Console.WriteLine("Inserted the training data for the model.");
-            Console.WriteLine("Training status: " + insertResponse.TrainingStatus);
-
-            // Wait until the training is complete
-            bool trainingRunning = true;
-            while (trainingRunning)
-            {
-                Console.WriteLine("Getting a new training progress status...");
-                var getRequest = predictionService.Trainedmodels.Get(PROJECT_NUMBER, PROJECT_ID);
-                var getResponse = getRequest.Execute();
-                Console.WriteLine("Got a new training progress status: {0}", getResponse.TrainingStatus);
-
-                switch (getResponse.TrainingStatus)
-                {
-                    case "RUNNING":
-                        Console.WriteLine("The model training is still in progress, let us wait for {0} ms.", PROGRESS_WAITING_TIME);
-                        Thread.Sleep(PROGRESS_WAITING_TIME);
-                        break;
-                    case "DONE":
-                        Console.WriteLine("The model has been trained successfully.");
-                        Insert2.ModelInfoData modelInfoData = getResponse.ModelInfo;
-                        if (modelInfoData != null)
+                        switch (getResponse.TrainingStatus)
                         {
-                            Console.WriteLine("ModelType: {0}", modelInfoData.ModelType);
-                            Console.WriteLine("NumberInstances: {0}", modelInfoData.NumberInstances);
-                            Console.WriteLine("MeanSquaredError: {0}", modelInfoData.MeanSquaredError);                            
+                            case "RUNNING":
+                                Console.WriteLine("The model training is still in progress, let us wait for {0} ms.", PROGRESS_WAITING_TIME);
+                                Thread.Sleep(PROGRESS_WAITING_TIME);
+                                break;
+                            case "DONE":
+                                Console.WriteLine("The model has been trained successfully.");
+                                Insert2.ModelInfoData modelInfoData = getResponse.ModelInfo;
+                                if (modelInfoData != null)
+                                {
+                                    Console.WriteLine("ModelType: {0}", modelInfoData.ModelType);
+                                    Console.WriteLine("NumberInstances: {0}", modelInfoData.NumberInstances);
+                                    Console.WriteLine("MeanSquaredError: {0}", modelInfoData.MeanSquaredError);
+                                }
+                                trainingRunning = false;
+                                break;
+                            case "ERROR: TRAINING JOB NOT FOUND":
+                                throw new Exception("the training job was not found.");
+                            case "ERROR: TOO FEW INSTANCES IN DATASET":
+                                throw new Exception("there are too few instances in the dataset.");
+                            default:
+                                throw new ArgumentException("Unknown status (error): " + getResponse.TrainingStatus);
                         }
-                        trainingRunning = false;
-                        break;
-                    case "ERROR: TRAINING JOB NOT FOUND":
-                        throw new Exception("the training job was not found.");
-                    case "ERROR: TOO FEW INSTANCES IN DATASET":
-                        throw new Exception("there are too few instances in the dataset.");
-                    default:
-                        throw new ArgumentException("Unknown status (error): " + getResponse.TrainingStatus);
-                }
+                    }
+                    break;
             }
-
             //predictionService.Trainedmodels.Predict(body, PROJECT_ID, Analyze.ModelDescriptionData)
         }
     }
