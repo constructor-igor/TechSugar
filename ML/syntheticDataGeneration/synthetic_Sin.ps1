416dd8f44ed0d33b871f9613ad8c3f038ac1fc8f
constructor-igor <igor.zc@gmail.com>
2016-06-14 12:55:24 +0300

    #156 updated powershell script

--- ML/syntheticDataGeneration/synthetic_Sin.ps1
+++ ML/syntheticDataGeneration/synthetic_Sin.ps1
@@ -1,13 +1,15 @@
 #
 #
 #
-$capacity = 10
+$capacity = 360*10
 $file = [string]::Format("sin-{0}-training-data.csv", $capacity)
 remove-item -path $file -force -ErrorAction:Ignore
 
 0..$capacity | 
-    % {$_/10} |
-    select @{Name="X";Expression={$_}}, @{Name="Y";Expression={[math]::Sin($_)}} | 
+#    % {$_/10} |
+    % {($_/180)*[math]::PI} |
+#    select @{Name="X";Expression={$_}}, @{Name="Y";Expression={[math]::Sin($_)}} | 
+    select @{Name="Y";Expression={[math]::Sin($_)}}, @{Name="X";Expression={$_}} | #google style Y, X
     convertto-csv -NoTypeInformation -Delimiter "," | 
     % {$_ -replace '"',''} |
     Out-File $file
