ee98c9ff5362bf51523256957baf89009f0500bf
constructor-igor <igor.zc@gmail.com>
2016-06-23 18:18:27 +0300

    #169 created ConvertCsvToExcel_MicrosoftOfficeInteropExcel

--- csharp-tips/csharp-tips/csharp-tips/Excel/ConvertCsvToExcel.cs
+++ csharp-tips/csharp-tips/csharp-tips/Excel/ConvertCsvToExcel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Windows.Forms;
 using NFile.Framework;
 using NUnit.Framework;
 using OfficeOpenXml;
@@ -36,6 +37,39 @@ namespace csharp_tips.Excel
                     package.Save();
                 }
                 Console.WriteLine("created file {0}", xlsxFile.FileName);
+            }
+        }
+
+        [Test, STAThread]
+        [Category("failed, created empty excel file")]
+        public void ConvertCsvToExcel_MicrosoftOfficeInteropExcel()
+        {
+            StringBuilder content = new StringBuilder();
+            content.AppendLine("param1\tparam2\tstatus");
+            content.AppendLine("0.5\t10\tpassed");
+            content.AppendLine("10\t20\tfail");
+
+            using (TemporaryFile xlsxFile = new TemporaryFile(".xlsx"))
+            {
+                Clipboard.SetText(content.ToString());
+                Microsoft.Office.Interop.Excel.Application xlexcel;
+                Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+                Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+                object misValue = System.Reflection.Missing.Value;
+                xlexcel = new Microsoft.Office.Interop.Excel.Application();
+                // for excel visibility
+                //xlexcel.Visible = true;
+                // Creating a new workbook
+                xlWorkBook = xlexcel.Workbooks.Add(misValue);
+                // Putting Sheet 1 as the sheet you want to put the data within
+                xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet) xlWorkBook.Worksheets.get_Item(1);
+                // creating the range
+                Microsoft.Office.Interop.Excel.Range CR = (Microsoft.Office.Interop.Excel.Range) xlWorkSheet.Cells[1, 1];
+                CR.Select();
+                xlWorkSheet.Paste(CR, false);
+                xlWorkSheet.SaveAs(xlsxFile.FileName);                
+                xlexcel.Quit();
+                Console.WriteLine("Created file {0}", xlsxFile.FileName);
             }
         }
     }
