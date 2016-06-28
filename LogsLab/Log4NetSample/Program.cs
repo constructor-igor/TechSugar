4023468e76933d8d5624231ec4785dfa964f8ecc
constructor-igor <igor.zc@gmail.com>
2016-06-28 13:03:02 +0300

    #171 first sample

---
+++ LogsLab/Log4NetSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+
+/*
+ *  References:
+ *  - https://csharp.today/log4net-tutorial-great-library-for-logging/
+ * 
+ * */
+
+namespace Log4NetSample
+{
+    class Program
+    {
+        private static readonly ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            Log.Info("your log message");
+        }
+    }
+}
