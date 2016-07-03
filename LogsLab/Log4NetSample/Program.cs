f728802b6f087c0c076ac24c445ece47d6d21336
constructor-igor <igor.zc@gmail.com>
2016-07-03 14:02:19 +0300

    #171 added sample of "named" log

--- LogsLab/Log4NetSample/Program.cs
+++ LogsLab/Log4NetSample/Program.cs
@@ -11,10 +11,13 @@ namespace Log4NetSample
     class Program
     {
         private static readonly ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly ILog InvestigationLog = log4net.LogManager.GetLogger("investigation");
 
         static void Main(string[] args)
         {
             Log.Info("your log message");
+            InvestigationLog.Info("investigate INFO");
+            InvestigationLog.Debug("investigate DEBUG");
         }
     }
 }
