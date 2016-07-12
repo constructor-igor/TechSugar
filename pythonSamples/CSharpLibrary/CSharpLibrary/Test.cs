1df3a4b9e8c25473617dac10c66bea710125850a
constructor-igor <igor.zc@gmail.com>
2016-07-12 18:32:08 +0300

    #173

--- pythonSamples/CSharpLibrary/CSharpLibrary/Test.cs
+++ pythonSamples/CSharpLibrary/CSharpLibrary/Test.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 using RGiesecke.DllExport;
 
 public class Test
@@ -11,11 +12,19 @@ public class Test
     [DllExport("AddArray", CallingConvention = CallingConvention.Cdecl)]
     public static int[] TestExportAddArray(int left, int right)
     {
-        return new int[]{left, right};
+        return new[]{left, right};
     }
-    [DllExport("AddString", CallingConvention = CallingConvention.Cdecl)]
-    public static string TestExportAddString(string left, string right)
+    [DllExport("AddString", CallingConvention = CallingConvention.Winapi)]
+    [return: MarshalAs(UnmanagedType.LPWStr)]
+    public static string TestExportAddString([MarshalAs(UnmanagedType.LPWStr)] string left, [MarshalAs(UnmanagedType.LPWStr)] string right)
     {
         return left + right;
+    }
+
+    [DllExport("GetGreeting", CallingConvention = CallingConvention.Winapi)]
+    [return: MarshalAs(UnmanagedType.LPWStr)]
+    public static String GetGreeting()
+    {
+        return "Happy Managed Coding!";
     }
 }
--- pythonSamples/callNetFromPython.py
+++ pythonSamples/callNetFromPython.py
@@ -1,6 +1,16 @@
 import ctypes
 source = 'CSharpLibrary.dll'
 a = ctypes.cdll.LoadLibrary(source)
+
 r = a.AddInt(3, 5)
-s = a.AddString('3', '5')
-a.AddArray(3,5)
+
+getGreeting = a.GetGreeting
+gResult = getGreeting()
+
+getGreeting.restype = ctypes.c_char_p
+gResult = getGreeting()
+
+a.str.restype = ctypes.c_wchar
+g = a.GetGreeting()
+s = a.AddString("3", "5")
+arr = a.AddArray(3,5)
