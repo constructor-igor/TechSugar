d27d825199bec36f0dc6a2612c659bac4ea0b0c1
constructor-igor <igor.zc@gmail.com>
2016-07-26 23:23:38 +0300

    #185 created project SelfHostingSample

---
+++ nancy/HelloWorld/SelfHostingSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace SelfHostingSample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var url = "http://127.0.0.1:9000";
+            //UrlReservations urlReservations = new UrlReservations {CreateAutomatically = true};
+            HostConfiguration hostConfiguration = new HostConfiguration {UrlReservations = {CreateAutomatically = true}};
+            using (var host = new NancyHost(new Uri(url), new DefaultNancyBootstrapper(), hostConfiguration))
+            {
+                host.Start();
+                Console.WriteLine("Nancy Server listening on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
