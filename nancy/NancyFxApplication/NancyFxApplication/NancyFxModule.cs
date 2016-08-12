7aaef40663de857a1f43a95a187ee4bb54ecfe5f
constructor-igor <igor.zc@gmail.com>
2016-08-12 18:10:05 +0300

    #184 Sample NancyFxModule

---
+++ nancy/NancyFxApplication/NancyFxApplication/NancyFxModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+
+namespace NancyFxApplication
+{
+    public class NancyFxModule: NancyModule
+    {
+        public NancyFxModule()
+        {
+            Get["/"] = param => base.View["Index.html"];
+
+            Get["/{Name}"] = param =>
+            {
+                dynamic viewBag = new DynamicDictionary();
+                viewBag.Name = param.Name;
+                return View["Hello.html", viewBag];
+            };
+        }
+    }
+}
