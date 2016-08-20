a7b5a771c653b10efe74b843a0088152144be71b
constructor-igor <igor.zc@gmail.com>
2016-08-21 00:31:00 +0300

    #205 updated sample

--- EPPlusSamples/EPPlusSamples/Program.cs
+++ EPPlusSamples/EPPlusSamples/Program.cs
@@ -1,36 +1,68 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using OfficeOpenXml;
 using OfficeOpenXml.Drawing.Chart;
+using OfficeOpenXml.Style;
 
 namespace EPPlusSamples
 {
     class Program
     {
+        private static readonly Color EMPTY_COLUMN_COLOR = Color.Gainsboro;
+        private static Color NUMBER_OF_FAILES = Color.Yellow;
+        private static Color NUMBER_OF_FAILES_IN_PROCENTS = Color.Red;
+
         static void Main(string[] args)
         {
-            //const string csvFile = @"..\..\dataFile.csv";
-            const string csvFile = @"..\..\realData.csv";
+            const string csvFile = @"..\..\dataFile.csv";
             const string excelFile = "Excel.xlsx";
             const string updatedExcelFile = "updatedExcel.xlsx";
-            //const string columnName = "C";
-            const string columnName = "AI";
+            const string columnName = "C";
             const string sheetName = "general data";
 
-            File.Delete(excelFile);
+            CreateExcelFile(excelFile, sheetName, csvFile, columnName);
 
-            // create excel file with chart
+            // open and save
+            OpenAndSave(updatedExcelFile, excelFile);
+        }
+
+        private static void OpenAndSave(string updatedExcelFile, string excelFile)
+        {
+            File.Delete(updatedExcelFile);
+            File.Copy(excelFile, updatedExcelFile);
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFile)))
+            {
+                package.SaveAs(new FileInfo(updatedExcelFile));
+            }
+        }
+
+        private static void CreateExcelFile(string excelFile, string sheetName, string csvFile, string columnName)
+        {
+            File.Delete(excelFile);
 
             FileInfo excelFileInfo = new FileInfo(excelFile);
             using (ExcelPackage package = new ExcelPackage(excelFileInfo))
             {
                 ExcelWorksheets workSheets = package.Workbook.Worksheets;
                 ExcelWorksheet dataWorkSheet = workSheets.Add(sheetName);
-                var format = new ExcelTextFormat { Delimiter = '\t', EOL = "\r" };
+                var format = new ExcelTextFormat {Delimiter = '\t', EOL = "\r"};
                 dataWorkSheet.Cells["A1"].LoadFromText(new FileInfo(csvFile), format);
+                int rowsCount = dataWorkSheet.Dimension.End.Row - 1;
+
+                ExcelColumn preColumn = dataWorkSheet.Column(2);
+                preColumn.Width = 2;
+                ExcelColumn postColumn = dataWorkSheet.Column(17);
+                postColumn.Width = 2;
+
+                for (int row = 0; row < rowsCount; row++)
+                {
+                    SetColor(dataWorkSheet.Cells[row + 1, 2], EMPTY_COLUMN_COLOR);
+                    SetColor(dataWorkSheet.Cells[row + 1, 17], EMPTY_COLUMN_COLOR);
+                }
 
                 ExcelWorksheet chartsWorksheet = workSheets.Add("Charts");
-                int rowsCount = dataWorkSheet.Dimension.End.Row - 1;
                 ExcelChart chart = chartsWorksheet.Drawings.AddChart("StdDev", eChartType.ColumnClustered);
                 chart.Title.Text = "StdDev";
                 chart.SetPosition(1, 0, 1, 0);
@@ -41,15 +73,12 @@ namespace EPPlusSamples
 
                 package.Save();
             }
+        }
 
-            // open and save
-            File.Delete(updatedExcelFile);
-            File.Copy(excelFile, updatedExcelFile);
-            using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFile)))
-            {
-                package.SaveAs(new FileInfo(updatedExcelFile));
-            }
-
+        private static void SetColor(ExcelRangeBase cell, Color color)
+        {
+            cell.Style.Fill.PatternType = ExcelFillStyle.Solid;
+            cell.Style.Fill.BackgroundColor.SetColor(color);
         }
     }
 }
