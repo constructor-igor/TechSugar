b90bda50491655961d2922d1827e925d34326c9f
constructor-igor <igor.zc@gmail.com>
2016-09-22 18:07:49 +0300

    #199 added "session" sample

--- nancy/HelloWorld/SelfHostingSample/Program.cs
+++ nancy/HelloWorld/SelfHostingSample/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Nancy;
+using Nancy.Bootstrapper;
 using Nancy.Hosting.Self;
+using Nancy.Session;
+using Nancy.TinyIoc;
 
 namespace SelfHostingSample
 {
@@ -11,11 +14,20 @@ namespace SelfHostingSample
             var url = "http://127.0.0.1:9000";
             //UrlReservations urlReservations = new UrlReservations {CreateAutomatically = true};
             HostConfiguration hostConfiguration = new HostConfiguration {UrlReservations = {CreateAutomatically = true}};
-            using (var host = new NancyHost(new Uri(url), new DefaultNancyBootstrapper(), hostConfiguration))
+            using (var host = new NancyHost(new Uri(url), new CustomBootstrapper(), hostConfiguration))
             {
                 host.Start();
                 Console.WriteLine("Nancy Server listening on {0}", url);
                 Console.ReadLine();
+            }
+        }
+
+        public class CustomBootstrapper : DefaultNancyBootstrapper
+        {
+            protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+            {
+                base.ApplicationStartup(container, pipelines);
+                CookieBasedSessions.Enable(pipelines);
             }
         }
     }
--- nancy/HelloWorld/SelfHostingSample/RootRoutes.cs
+++ nancy/HelloWorld/SelfHostingSample/RootRoutes.cs
@@ -6,6 +6,7 @@ namespace SelfHostingSample
 {
     public class RootRoutes : NancyModule
     {
+        const string nameMessageKey = "name";
         public RootRoutes()
         {
             Get["/"] = Index;
@@ -16,7 +17,8 @@ namespace SelfHostingSample
 
         private dynamic Index(dynamic parameters)
         {
-            return "Hello World";
+            string message = Session[nameMessageKey] != null ? Session[nameMessageKey].ToString() : "World";
+            return String.Format("Hello {0}", message);
         }
 
         private dynamic JsonTest(dynamic parameters)
@@ -28,6 +30,7 @@ namespace SelfHostingSample
         private dynamic HelloName(dynamic parameters)
         {
             var name = parameters.name;
+            Session[nameMessageKey] = name.ToString();
             return String.Format("Hello there {0}", name);
         }
 
