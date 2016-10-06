ffd6c03b0cb2648eb01dfadf1c70058a711ba879
constructor-igor <igor.zc@gmail.com>
2016-10-06 12:04:48 +0300

    added test for https://github.com/nunit/nunit/issues/1825

---
+++ TestingLab/NUnitSamples/NUnit_v3_samples/NUnitCollectionTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace NUnit_v3_samples
+{
+    [TestFixture]
+    public class NUnitCollectionTests
+    {
+        [Test]
+        public void Test()
+        {
+            byte[,] actual = {
+                {10, 20, 30, 20},
+                {11, 21, 31, 41}
+            };
+            byte[,] expected = {
+                {10, 20, 30, 20},
+                {11, 21, 31, 41}
+            };
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AreEqual_1024()
+        {
+            byte[,] actual = new byte[1024, 1024];
+            byte[,] expected = new byte[1024, 1024];
+            
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void EquivalentTo_1024()
+        {
+            byte[,] actual = new byte[1024, 1024];
+            byte[,] expected = new byte[1024, 1024];
+
+            Assert.That(actual, Is.EquivalentTo(expected));
+        }
+    }
+}
---
+++ TestingLab/NUnitSamples/runTest_v3.cmd
@@ -0,0 +1,15 @@
+﻿rem
+rem
+rem
+rem D:\My\MyProjects\@NUnit\NUnit-3.4.1\bin\nunit3-console.exe
+rem pause
+
+rem	-labels=on
+rem	-labels=off
+rem	-labels=Before
+rem	-labels=After
+rem	-labels=All
+
+rem D:\My\@gh\nunit\nunit-console\bin\Debug\nunit3-console.exe 
+D:\My\@gh\nunit\nunit-console\bin\Debug\nunit3-console.exe NUnit_v3_samples\bin\debug\NUnit_v3_samples.dll -p:par2=3,4 --test=NUnit_v3_samples.NUnitIssue1738 --labels=After
+pause
