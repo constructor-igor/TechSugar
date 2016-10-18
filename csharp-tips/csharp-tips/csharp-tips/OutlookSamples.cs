70223978cc4ca0ee59322be281a4dc40d0291eae
constructor-igor <igor.zc@gmail.com>
2016-10-18 17:11:22 +0300

    #33 "The .NET weak event pattern" sample implementation

--- csharp-tips/csharp-tips/csharp-tips/OutlookSamples.cs
+++ csharp-tips/csharp-tips/csharp-tips/OutlookSamples.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-using Outlook = Microsoft.Office.Interop.Outlook;
+//using Outlook = Microsoft.Office.Interop.Outlook;
 
 namespace csharp_tips
 {
@@ -9,12 +9,12 @@ namespace csharp_tips
         [Test, Explicit]
         public void SendEmail()
         {
-            Outlook.Application application = new Outlook.ApplicationClass();
-            Outlook.MailItem mail = application.CreateItem(Outlook.OlItemType.olMailItem) as Outlook.MailItem;
-            mail.Subject = "subject";
-            Outlook.AddressEntry currentUser = application.Session.CurrentUser.AddressEntry;
-            mail.Recipients.Add(currentUser.Name);
-            mail.Send();
+//            Outlook.Application application = new Outlook.ApplicationClass();
+//            Outlook.MailItem mail = application.CreateItem(Outlook.OlItemType.olMailItem) as Outlook.MailItem;
+//            mail.Subject = "subject";
+//            Outlook.AddressEntry currentUser = application.Session.CurrentUser.AddressEntry;
+//            mail.Recipients.Add(currentUser.Name);
+//            mail.Send();
         }
     }
 }
---
+++ csharp-tips/csharp-tips/csharp-tips/ServiceBusSamples.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace csharp_tips
+{
+    [TestFixture]
+    public class ServiceBusSamples
+    {
+        [Test]
+        public void Intro()
+        {
+            //var messageFactory = MessagingFactory.CreateFromConnectionString(connectionString);
+        }
+    }
+}
