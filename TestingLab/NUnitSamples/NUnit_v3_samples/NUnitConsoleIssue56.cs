1f6b67222ce0e98e5cd6c4cd93b102c1782bb683
constructor-igor <igor.zc@gmail.com>
2016-10-30 17:26:46 +0200

    #236 added tests

--- TestingLab/NUnitSamples/NUnit_v3_samples/NUnitConsoleIssue56.cs
+++ TestingLab/NUnitSamples/NUnit_v3_samples/NUnitConsoleIssue56.cs
@@ -1,53 +1,73 @@
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 namespace NUnit_v3_samples
 {
     namespace Issue56
     {
-        namespace NamepSpace1
+        [TestFixture]
+        public class Category1Test
         {
-            [TestFixture]
-            public class Categoty1Test
+            [Test]
+            public void category1_test_1()
             {
-                [Test]
-                public void category1_test_1()
-                {
-                    Assert.Pass();
-                }
-                [Test]
-                public void category1_test_2()
-                {
-                    Assert.Pass();
-                }
-            }            
+                Assert.Pass();
+            }
+
+            [Test]
+            public void category1_test_2()
+            {
+                Assert.Pass();
+            }
         }
 
-        namespace NameSpace21
+        namespace NameSpace2
         {
-            namespace NameSpace22
+            [TestFixture]
+            public class TestSuite1Test
             {
-                [TestFixture]
-                public class TestSuite1Test
+                [Test]
+                public void testsuite1_test_1()
                 {
-                    [Test]
-                    public void testsuite1_test_1()
-                    {
-                        Assert.Pass();
-                    }
+                    Assert.Pass();
+                }
 
-                    [Test]
-                    public void testsuite1_test_2()
-                    {
-                        Assert.Pass();
-                    }
+                [Test]
+                public void testsuite1_test_2()
+                {
+                    Assert.Pass();
+                }
 
-                    [Test]
-                    public void testsuite1_test_3()
-                    {
-                        Assert.Pass();
-                    }
+                [Test]
+                public void testsuite1_test_3()
+                {
+                    Assert.Pass();
                 }
             }
+        }
+    }
+
+    [TestFixture]
+    public class RegexpIssue56Tests
+    {
+        [TestCase("NUnit_v3_samples.Issue56.Category1Test.category1_test_1", true)]
+        [TestCase("NUnit_v3_samples.Issue56.Category1Test.category1_test_2", true)]
+        [TestCase("NUnit_v3_samples.Issue56.NameSpace2.TestSuite1Test.testsuite1_test_1", false)]
+        [TestCase("NUnit_v3_samples.Issue56.NameSpace2.TestSuite1Test.testsuite1_test_2", false)]
+        [TestCase("NUnit_v3_samples.Issue56.NameSpace2.TestSuite1Test.testsuite1_test_3", false)]
+        public void Test(string fullTestName, bool expected)
+        {
+            Regex regex = new Regex(@"NUnit_v3_samples.Issue56.\w+.\w+$");
+            Match match = regex.Match(fullTestName);
+            Assert.That(match.Success, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Investigation()
+        {
+            Regex regex = new Regex(@"NUnit_v3_samples.Issue56.\w+.\w+$");
+            Match match = regex.Match("NUnit_v3_samples.Issue56.Category1Test.category1_test_1");
+            Assert.That(match.Success, Is.True);
         }
     }
 }
