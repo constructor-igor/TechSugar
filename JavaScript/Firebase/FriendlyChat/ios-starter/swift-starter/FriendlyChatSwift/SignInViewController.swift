e5a5145639cc897d97bcfbf12cdd31f14da77b13
constructor-igor <igor.zc@gmail.com>
2016-11-20 16:33:33 +0200

    #247 cloned Friendlychat step by step sample

---
+++ JavaScript/Firebase/FriendlyChat/ios-starter/swift-starter/FriendlyChatSwift/SignInViewController.swift
@@ -0,0 +1,54 @@
+//
+//  Copyright (c) 2015 Google Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+import Firebase
+
+@objc(SignInViewController)
+class SignInViewController: UIViewController {
+
+  @IBOutlet weak var emailField: UITextField!
+  @IBOutlet weak var passwordField: UITextField!
+
+  override func viewDidAppear(_ animated: Bool) {
+  }
+
+  @IBAction func didTapSignIn(_ sender: AnyObject) {
+    signedIn(nil)
+  }
+
+  @IBAction func didTapSignUp(_ sender: AnyObject) {
+    setDisplayName(nil)
+  }
+
+  func setDisplayName(_ user: FIRUser?) {
+    signedIn(nil)
+  }
+
+  @IBAction func didRequestPasswordReset(_ sender: AnyObject) {
+  }
+
+  func signedIn(_ user: FIRUser?) {
+    MeasurementHelper.sendLoginEvent()
+
+    AppState.sharedInstance.signedIn = true
+    let notificationName = Notification.Name(rawValue: Constants.NotificationKeys.SignedIn)
+    NotificationCenter.default.post(name: notificationName, object: nil, userInfo: nil)
+    performSegue(withIdentifier: Constants.Segues.SignInToFp, sender: nil)
+  }
+
+}
