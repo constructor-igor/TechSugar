b5cef3a5f88b84ef0cf3920ce3e0315b510e983a
constructor-igor <igor.zc@gmail.com>
2016-11-20 13:59:17 +0200

    added LINQ TakeSample sample

---
+++ csharp-tips/csharp-tips/csharp-tips/LINQ/TakeSample.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace csharp_tips.LINQ
+{
+    [TestFixture]
+    public class TakeSample
+    {
+        [Test]
+        public void TestEnoughData()
+        {
+            int[] data = Enumerable.Range(0, 10).ToArray();
+            int[] part = data.Take(5).ToArray();
+            Assert.That(part.Length, Is.EqualTo(5));
+        }
+        [Test]
+        public void TestNotEnoughData()
+        {
+            int[] data = Enumerable.Range(0, 10).ToArray();
+            int[] part = data.Take(15).ToArray();
+            Assert.That(part.Length, Is.EqualTo(10));
+        }
+    }
+}
