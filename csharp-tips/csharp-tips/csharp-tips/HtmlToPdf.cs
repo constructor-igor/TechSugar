d4b4f30c2c6d66669aef27a6425185521695d698
constructor-igor <igor.zc@gmail.com>
2016-11-28 22:48:01 +0200

    #250 added iTextSharp sample

--- csharp-tips/csharp-tips/csharp-tips/HtmlToPdf.cs
+++ csharp-tips/csharp-tips/csharp-tips/HtmlToPdf.cs
@@ -1,5 +1,8 @@
 using System;
 using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
 using NUnit.Framework;
 
 namespace csharp_tips
@@ -14,6 +17,7 @@ namespace csharp_tips
             string targetPdfFile = String.Format(@"..\..\htmlSamples\{0}.pdf", htmlFileName);
             File.WriteAllBytes(targetPdfFile, PdfSharpConvert(File.ReadAllText(sourceHtmlFile)));
         }
+
         public static Byte[] PdfSharpConvert(String html)
         {
             Byte[] res = null;
@@ -24,6 +28,57 @@ namespace csharp_tips
                 res = ms.ToArray();
             }
             return res;
+        }
+
+    }
+
+    [TestFixture]
+    public class HtmlToPdf_iTextSharp
+    {
+        [TestCase("BookIndex")]
+        public void DemoTest(string htmlFileName)
+        {
+            string sourceHtmlFile = String.Format(@"..\..\htmlSamples\{0}.html", htmlFileName);
+            string targetPdfFile = String.Format(@"..\..\htmlSamples\{0}.itextsharp.pdf", htmlFileName);
+            File.WriteAllBytes(targetPdfFile, CreatePDF(File.ReadAllText(sourceHtmlFile)).ToArray());
+        }
+//        [Test]
+//        public void ProductionTest(string htmlFileName)
+//        {
+//            string sourceHtmlFile = @"<source html>";
+//            string targetPdfFile = @"<target.pdf>";
+//            File.WriteAllBytes(targetPdfFile, CreatePDF(File.ReadAllText(sourceHtmlFile)).ToArray());
+//        }
+
+        private MemoryStream CreatePDF(string html)
+        {
+            MemoryStream msOutput = new MemoryStream();
+            TextReader reader = new StringReader(html);
+
+            // step 1: creation of a document-object
+            Document document = new Document(PageSize.A4, 30, 30, 30, 30);
+
+            // step 2:
+            // we create a writer that listens to the document
+            // and directs a XML-stream to a file
+            PdfWriter writer = PdfWriter.GetInstance(document, msOutput);
+
+            // step 3: we create a worker parse the document
+            HTMLWorker worker = new HTMLWorker(document);
+
+            // step 4: we open document and start the worker on the document
+            document.Open();
+            worker.StartDocument();
+
+            // step 5: parse the html into the document
+            worker.Parse(reader);
+
+            // step 6: close the document and the worker
+            worker.EndDocument();
+            worker.Close();
+            document.Close();
+
+            return msOutput;
         }
     }
 }
