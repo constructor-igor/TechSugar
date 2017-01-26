58bde0fddf3fd41bec5d64fed76d21204b0da617
constructor-igor <igor.zc@gmail.com>
2017-01-26 10:19:46 +0200

    #271 created ForEachInvestigation

---
+++ csharp-tips/csharp-tips/csharp-tips/LINQ/ForEachInvestigation.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace csharp_tips.LINQ
+{
+    /*
+     * References:
+     * - http://stackoverflow.com/questions/11179156/how-is-foreach-implemented-in-c
+     * - http://stackoverflow.com/questions/398982/how-do-foreach-loops-work-in-c
+     * 
+     * */
+
+    [TestFixture]
+    public class ForEachInvestigation
+    {
+        [Test]
+        public void ForEachForArray()
+        {
+            int[] dataArray = Enumerable.Range(0, 10).ToArray();
+            foreach (int dataItem in dataArray)
+            {
+                Console.WriteLine(dataItem);
+            }
+        }
+        [Test]
+        public void ForEachForEnumarable()
+        {
+            IEnumerable<int> dataEnumerable = Enumerable.Range(0, 10);
+            foreach (int dataItem in dataEnumerable)
+            {
+                Console.WriteLine(dataItem);
+            }
+        }
+    }
+}
