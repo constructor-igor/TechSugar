0f2160f6bf00410e204efcd11cee96a4d6423477
constructor-igor <igor.zc@gmail.com>
2017-02-01 15:25:43 +0200

    #284 add sample WordCounter.scala

---
+++ ScalaSamples/WordCounterSample/WordCounter.scala
@@ -0,0 +1,17 @@
+package main
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object WordCounter {
+	def main(args: Array[String]) {
+		val conf = new SparkConf().setAppName("Word Counter")
+		val sc = new SparkContext(conf)
+		val textFile = sc.textFile("file:///Spark/README.md")
+		val tokenizedFileData = textFile.flatMap(line=>line.split(" "))
+		val countPrep = tokenizedFileData.map(word=>(word, 1))
+		val counts = countPrep.reduceByKey((accumValue, newValue)=>accumValue + newValue)
+		val sortedCounts = counts.sortBy(kvPair=>kvPair._2, false)
+		sortedCounts.saveAsTextFile("file:///PluralsightData/ReadMeWordCountViaApp")
+	}
+}
