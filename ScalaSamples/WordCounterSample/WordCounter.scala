279f0e939353e21c7356f7d3a252ccf285271c23
constructor-igor <igor.zc@gmail.com>
2017-02-06 14:40:31 +0200

    #284 changed path to input file

--- ScalaSamples/WordCounterSample/WordCounter.scala
+++ ScalaSamples/WordCounterSample/WordCounter.scala
@@ -8,7 +8,8 @@ object WordCounter {
 	def main(args: Array[String]) {
 		val conf = new SparkConf().setAppName("Word Counter")
 		val sc = new SparkContext(conf)
-		val textFile = sc.textFile("file:///Spark/README.md")
+		//val textFile = sc.textFile("file:///Spark/README.md")
+		val textFile = sc.textFile("file:///README.md")
 		val tokenizedFileData = textFile.flatMap(line=>line.split(" "))
 		val countPrep = tokenizedFileData.map(word=>(word, 1))
 		val counts = countPrep.reduceByKey((accumValue, newValue)=>accumValue + newValue)
