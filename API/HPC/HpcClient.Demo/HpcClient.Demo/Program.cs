6c77ed426641c2e64f16e8be96e20b4dbc896475
constructor-igor <igor.zc@gmail.com>
2017-02-14 15:35:09 +0200

    created sample HpcCleint.Demo

---
+++ API/HPC/HpcClient.Demo/HpcClient.Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Hpc.Scheduler;
+
+namespace HpcClient.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] hpcNames = { "server1", "server2" };
+
+            Scheduler scheduler = new Scheduler();
+            foreach (string serverPath in hpcNames)
+            {
+                try
+                {
+                    scheduler.Connect(serverPath);
+                    //Console.WriteLine("connected to {0}", serverPath);
+
+                    ISchedulerCounters schedulerCounters = scheduler.GetCounters();
+                    int runningJobs = schedulerCounters.RunningJobs;
+                    Console.WriteLine("[{0}] running jobs (queue): {1}", serverPath, runningJobs);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("[{0}] Not accessible", serverPath);
+                }
+            }
+       }
+    }
+}
