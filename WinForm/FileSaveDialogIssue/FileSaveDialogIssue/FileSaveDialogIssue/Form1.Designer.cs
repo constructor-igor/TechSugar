3c8823e657c2f547b7f5d43ae31a11b7fc325d41
constructor-igor <igor.zc@gmail.com>
2017-02-23 15:05:08 +0200

    FileSaveDialogIssue: added 2nd button

--- WinForm/FileSaveDialogIssue/FileSaveDialogIssue/FileSaveDialogIssue/Form1.Designer.cs
+++ WinForm/FileSaveDialogIssue/FileSaveDialogIssue/FileSaveDialogIssue/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.btnOpenFile = new System.Windows.Forms.Button();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOpenFile
@@ -37,17 +38,29 @@
             this.btnOpenFile.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOpenFile.Location = new System.Drawing.Point(44, 65);
             this.btnOpenFile.Name = "btnOpenFile";
-            this.btnOpenFile.Size = new System.Drawing.Size(138, 48);
+            this.btnOpenFile.Size = new System.Drawing.Size(576, 48);
             this.btnOpenFile.TabIndex = 0;
-            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.Text = "Select File (*.BMP;*.MGM;*.PNG)";
             this.btnOpenFile.UseVisualStyleBackColor = true;
             this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(44, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(576, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select File (*.bmp;*.mgm;*.png)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(789, 325);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnOpenFile);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -59,6 +72,7 @@
 
         private System.Windows.Forms.Button btnOpenFile;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WinForm/FileSaveDialogIssue/FileSaveDialogIssue/FileSaveDialogIssue/Form1.cs
+++ WinForm/FileSaveDialogIssue/FileSaveDialogIssue/FileSaveDialogIssue/Form1.cs
@@ -9,13 +9,28 @@ namespace FileSaveDialogIssue
         {
             InitializeComponent();
             this.saveFileDialog.AddExtension = false;
+            this.saveFileDialog.SupportMultiDottedExtensions = false;
+            this.saveFileDialog.CheckFileExists = false;
         }
 
         private void btnOpenFile_Click(object sender, EventArgs e)
         {
             saveFileDialog.Title = @"Save calculated image";
             saveFileDialog.FileName = "noname.tiff";
+            //saveFileDialog.Filter = @"My Files(*.BMP;*.MGM;*.PNG)|*.BMP;*.MGM;*.PNG|All files (*.*)|*.*";
             saveFileDialog.Filter = @"My Files(*.BMP;*.MGM;*.PNG)|*.BMP;*.MGM;*.PNG|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                MessageBox.Show(String.Format("File name {0}", saveFileDialog.FileName));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.Title = @"Save calculated image";
+            saveFileDialog.FileName = "noname.tiff";
+            //saveFileDialog.Filter = @"My Files(*.BMP;*.MGM;*.PNG)|*.BMP;*.MGM;*.PNG|All files (*.*)|*.*";
+            saveFileDialog.Filter = @"My Files(*.BMP;*.MGM;*.PNG)|*.bmp;*.mgm;*.png|All files (*.*)|*.*";
             if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
             {
                 MessageBox.Show(String.Format("File name {0}", saveFileDialog.FileName));
