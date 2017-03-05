3843fa426bda60e6d2b16196b2fd9e1b54e62ae0
constructor-igor <igor.zc@gmail.com>
2017-03-05 22:06:14 +0200

    #298 created ConsoleApp (including sample of test)

---
+++ JavaLab/ConsoleApp/src/com/company/Person.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by igor-z on 05-Mar-17.
+ */
+public class Person {
+    private String name;
+    public String getName(){
+        return this.name;
+    }
+    public Person(String name){
+        this.name = name;
+    }
+}
