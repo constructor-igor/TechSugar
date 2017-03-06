26b920e966762e8ef58e6fbe287fc01048f86749
constructor-igor <igor.zc@gmail.com>
2017-03-06 09:03:04 +0200

    #298 created ConsoleApp (including sample of test)

---
+++ JavaLab/ConsoleApp/tests/com.company/PersonTest.java
@@ -0,0 +1,17 @@
+package com.company;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by igor-z on 05-Mar-17.
+ */
+class PersonTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+        assertTrue(true);
+    }
+
+}
