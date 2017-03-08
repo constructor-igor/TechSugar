5dd2e0a8cea96047f42ada8b7879aed7141513c8
constructor-igor <igor.zc@gmail.com>
2017-03-09 00:55:36 +0200

    implemented java solution on problem #8

--- projecteuler/0008/problem_0008_java/src/main/java/com/mycompany/app/App.java
+++ projecteuler/0008/problem_0008_java/src/main/java/com/mycompany/app/App.java
@@ -76,51 +76,50 @@ public class App
     }
     public static void SoveProblemCase3(){
         int numberOfDigits = 13;
-        StringBuffer buffer= new StringBuffer();
-        buffer.append("73167176531330624919225119674426574742355349194934");
-        buffer.append("96983520312774506326239578318016984801869478851843");
-        buffer.append("85861560789112949495459501737958331952853208805511");
-        buffer.append("12540698747158523863050715693290963295227443043557");
-        buffer.append("66896648950445244523161731856403098711121722383113");
-        buffer.append("62229893423380308135336276614282806444486645238749");
-        buffer.append("30358907296290491560440772390713810515859307960866");
-        buffer.append("70172427121883998797908792274921901699720888093776");
-        buffer.append("65727333001053367881220235421809751254540594752243");
-        buffer.append("52584907711670556013604839586446706324415722155397");
-        buffer.append("53697817977846174064955149290862569321978468622482");
-        buffer.append("83972241375657056057490261407972968652414535100474");
-        buffer.append("82166370484403199890008895243450658541227588666881");
-        buffer.append("16427171479924442928230863465674813919123162824586");
-        buffer.append("17866458359124566529476545682848912883142607690042");
-        buffer.append("24219022671055626321111109370544217506941658960408");
-        buffer.append("07198403850962455444362981230987879927244284909188");
-        buffer.append("84580156166097919133875499200524063689912560717606");
-        buffer.append("05886116467109405077541002256983155200055935729725");
-        buffer.append("71636269561882670428252483600823257530420752963450");
-        SolveProblem(numberOfDigits, buffer.toString());
+        StringBuilder builder = new StringBuilder()
+            .append("73167176531330624919225119674426574742355349194934")
+            .append("96983520312774506326239578318016984801869478851843")
+            .append("85861560789112949495459501737958331952853208805511")
+            .append("12540698747158523863050715693290963295227443043557")
+            .append("66896648950445244523161731856403098711121722383113")
+            .append("62229893423380308135336276614282806444486645238749")
+            .append("30358907296290491560440772390713810515859307960866")
+            .append("70172427121883998797908792274921901699720888093776")
+            .append("65727333001053367881220235421809751254540594752243")
+            .append("52584907711670556013604839586446706324415722155397")
+            .append("53697817977846174064955149290862569321978468622482")
+            .append("83972241375657056057490261407972968652414535100474")
+            .append("82166370484403199890008895243450658541227588666881")
+            .append("16427171479924442928230863465674813919123162824586")
+            .append("17866458359124566529476545682848912883142607690042")
+            .append("24219022671055626321111109370544217506941658960408")
+            .append("07198403850962455444362981230987879927244284909188")
+            .append("84580156166097919133875499200524063689912560717606")
+            .append("05886116467109405077541002256983155200055935729725")
+            .append("71636269561882670428252483600823257530420752963450");
+        SolveProblem(numberOfDigits, builder.toString());
     }
 
     public static long SolveProblem(int numberOfDigits, String input){
         long maxProduct = Integer.MIN_VALUE;
 
-        byte[] allDigits = input.replace("\n", "").getBytes();
+        final byte[] allDigits = input.getBytes();
         int codeZero = '0' & 0x00FF;
         byte codeByteZero = (byte) codeZero;
         for (int i = 0; i < allDigits.length; i++) {
             allDigits[i] = (byte)(allDigits[i] - codeByteZero);
         }
-        System.out.println("Size of allDigits: " + allDigits.length);
+        System.out.println(String.format("Size of allDigits: %d", allDigits.length));
 
         for (int i = 0; i < allDigits.length - numberOfDigits + 1; i++){
+
             long currentProduct = 1;
-            for (int j = 0; j < numberOfDigits; j++){
-                currentProduct *= allDigits[i+j];
-            }
+            for (int j = 0; j < numberOfDigits; j++) currentProduct *= allDigits[i + j];
             if (currentProduct > maxProduct){
                 maxProduct = currentProduct;
             }
         }
-        System.out.println("Max product: " + maxProduct);
+        System.out.println(String.format("Max product: %d", maxProduct));
         return maxProduct;
     }
 }
--- projecteuler/0008/problem_0008_java/src/test/java/com/mycompany/app/AppTest.java
+++ projecteuler/0008/problem_0008_java/src/test/java/com/mycompany/app/AppTest.java
@@ -1,36 +1,35 @@
 package com.mycompany.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
+//
+//    /**
+//     * Rigourous Test :-)
+//     */
+    @Test
     public void testApp()
     {
         long maxProduct = App.SolveProblem(2, "01234567899");
