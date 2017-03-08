20bcf8fff6da8f255624430524af60935361d00e
constructor-igor <igor.zc@gmail.com>
2017-03-08 23:54:09 +0200

    implemented java solution on problem #8

---
+++ projecteuler/0008/problem_0008_java/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,39 @@
+package com.mycompany.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        long maxProduct = App.SolveProblem(2, "01234567899");
+        assertEquals(81, maxProduct);
+    }
+}
