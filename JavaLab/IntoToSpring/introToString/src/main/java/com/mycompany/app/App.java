fe8fc70555b13a0f72848cdfc62f17328927bfc3
constructor-igor <igor.zc@gmail.com>
2017-03-14 21:22:19 +0200

    #300 DI via "set" and "constructor"

--- JavaLab/IntoToSpring/introToString/src/main/java/com/mycompany/app/App.java
+++ JavaLab/IntoToSpring/introToString/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,8 @@
 package com.mycompany.app;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,20 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        SequenceGenerator generator1 = (SequenceGenerator) context.getBean("sequenceGenerator");
+        System.out.println("sequenceGenerator");
+        System.out.println(generator1.getSequence());
+        System.out.println(generator1.getSequence());
+        System.out.println(generator1.getSequence());
+
+        System.out.println("sequenceGeneratorCtor");
+        SequenceGenerator generator2 = (SequenceGenerator) context.getBean("sequenceGeneratorCtor");
+        System.out.println(generator2.getSequence());
+        System.out.println(generator2.getSequence());
+        System.out.println(generator2.getSequence());
+
     }
 }
---
+++ JavaLab/IntoToSpring/introToString/src/main/java/com/mycompany/app/SequenceGenerator.java
@@ -0,0 +1,40 @@
+package com.mycompany.app;
+
+import java.util.List;
+
+/**
+ * Created by igor-z on 13-Mar-17.
+ */
+public class SequenceGenerator {
+    private String prefix;
+    private List<Object> suffixes;
+    private int initial;
+    private int counter;
+    public SequenceGenerator() { }
+    public SequenceGenerator(String prefix, List<Object> suffixes, int initial) {
+        this.prefix = prefix;
+        this.suffixes = suffixes;
+        this.initial = initial;
+    }
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public void setSuffixes(List<Object> suffixes) {
+        this.suffixes = suffixes;
+    }
+
+    public void setInitial(int initial) {
+        this.initial = initial;
+    }
+    public synchronized String getSequence() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(prefix);
+        buffer.append(initial + "-" + counter++);
+        for (Object suffix : suffixes) {
+            buffer.append("-");
+            buffer.append(suffix);
+        }
+        return buffer.toString();
+    }
+}
