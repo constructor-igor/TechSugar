66579a757aeeaf62de1b15c1c59033e0c9712771
constructor-igor <igor.zc@gmail.com>
2017-05-27 23:17:40 +0300

    #339 sample created

---
+++ LogsLab/NlogSample/NlogSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using NLog;
+using Project1;
+using Project2;
+
+namespace NlogSample
+{
+    class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Info("Main started");
+            Project1Library library1 = new Project1Library();
+            Project2Library library2 = new Project2Library();
+
+            library1.Run();
+            library1.Run();
+            library2.Run();
+
+            logger.Info("Main complted");
+        }
+    }
+}
---
+++ LogsLab/NlogSample/Project2/Project2Library.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace Project2
+{
+    public class Project2Library
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public Project2Library()
+        {
+            logger.Trace("Project2Library created");
+        }
+
+        public void Run()
+        {
+            logger.Info("Project2Library.run()");
+        }
+
+    }
+}
