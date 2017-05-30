7f5e9049e0a1b4ff7e83e98dc8db03e222e5ae69
constructor-igor <igor.zc@gmail.com>
2017-05-30 08:21:16 +0300

    #338 sample from course

---
+++ ScalaSamples/scala-spark-big-data/GettingStartedTask/example/src/main/scala/App.scala
@@ -0,0 +1,8 @@
+import example.Lists
+
+/**
+  * Created by igor-z on 29-May-17.
+  */
+object Main extends App {
+    println(Lists.max(List(1,3,2)))
+}
--- ScalaSamples/scala-spark-big-data/example/build.sbt
+++ ScalaSamples/scala-spark-big-data/example/build.sbt
@@ -3,4 +3,3 @@ name := "example"
 version := "1.0"
 
 scalaVersion := "2.12.2"
-        
