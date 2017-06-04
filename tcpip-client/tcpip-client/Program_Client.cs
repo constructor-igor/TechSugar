2fa77a1fef4769d096cc8e88c1826bcc54abad5d
constructor-igor <igor.zc@gmail.com>
2017-06-04 14:55:12 +0300

    #342 added "Timing Attack" sample

---
+++ tcpip-client/tcpip-client/Program_Client.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace tcpip_client
+{
+    class Program_Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("client started...");
+//            Connect("127.0.0.1", 13000, "hello");
+            TimingAttack("127.0.0.1", 13000);
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+        static void Connect(String server, Int32 port, String message)
+        {
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                //Int32 port = 13000;
+                using (TcpClient client = new TcpClient(server, port))
+                {
+                    // Translate the passed message into ASCII and store it as a Byte array.
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                    // Get a client stream for reading and writing.
+                    //  Stream stream = client.GetStream();
+
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        // Send the message to the connected TcpServer. 
+                        stream.Write(data, 0, data.Length);
+
+                        Console.WriteLine("Sent: {0}", message);
+
+                        // Receive the TcpServer.response.
+
+                        // Buffer to store the response bytes.
+                        data = new Byte[256];
+
+                        // String to store the response ASCII representation.
+                        String responseData = String.Empty;
+
+                        // Read the first batch of the TcpServer response bytes.
+                        Int32 bytes = stream.Read(data, 0, data.Length);
+                        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                        Console.WriteLine("Received: {0}", responseData);
+                    }
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+
+        static void TimingAttack(String server, Int32 port)
+        {
+            using (TcpClient client = new TcpClient(server, port))
+            using (NetworkStream stream = client.GetStream())
+            {
+                List<char> possiblePassowrdChars = new List<char>();
+                possiblePassowrdChars.AddRange(Enumerable.Range(0, 10).Select(i=>Convert.ToChar('0' + i)));
+                possiblePassowrdChars.AddRange(Enumerable.Range(0, 26).Select(i=>Convert.ToChar('a' + i)));
+//                possiblePassowrdChars.AddRange(Enumerable.Range(0, 26).Select(i=>Convert.ToChar('A' + i)));
+
+                TimeSpan maxSpan = TimeSpan.Zero;
+                Char foundKeyChar = '.';
+                string foundPassword = "";
+                const int sizeOfPassword = 8;
+                foreach (int sizeOfNotFound in Enumerable.Range(1, sizeOfPassword).Reverse())
+                {
+                    foreach (char passwordChar in possiblePassowrdChars)
+                    {
+                        string message = "password:" + foundPassword + passwordChar + String.Join("", Enumerable.Repeat(".", sizeOfNotFound-1));
+                        Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                        Byte[] answer = new Byte[256];
+                        int sizeOfAnswer = 0;
+                        String responseData = "";
+
+                        TimeSpan current = TimeSpan.Zero;
+                        int repeaterForEachChar = 10;
+                        for (int i = 0; i < repeaterForEachChar; i++)
+                        {
+                            Stopwatch stop = new Stopwatch();
+                            stop.Start();
+                            stream.Write(data, 0, data.Length);
+                            sizeOfAnswer = stream.Read(answer, 0, data.Length);
+                            stop.Stop();
+                            if (i!=0)   // for warm
+                                current += stop.Elapsed;
+                        }
+                        responseData = System.Text.Encoding.ASCII.GetString(answer, 0, sizeOfAnswer);
+                        if (current > maxSpan)
+                        {
+                            maxSpan = current;
+                            foundKeyChar = passwordChar;
+                        }
+                        Console.WriteLine("For {0} received: {1}, Elapsed = {2}", message, responseData, current);
+                        if (responseData == "True")
+                        {
+                            Console.WriteLine("Found password: {0}", message);
+                            break;
+                        }
+                    }
+                    foundPassword += foundKeyChar;
+                    Console.WriteLine("Found char: {0}", foundKeyChar);                    
+                    Console.WriteLine("Found password: {0}", foundPassword);                    
+                }                
+            }
+        }
+    }
+}
---
+++ tcpip-client/tcpip-server/Program_Server.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace tcpip_server
+{
+    class Program_Server
+    {
+        const string password_key = "password:";
+        const string passsword = "qwerty19";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("server started...");
+
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+//                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+//                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+//                        Console.WriteLine("Received: {0}", data);
+
+                        var answer = data.StartsWith(password_key) ? CheckPassowrd(data.Remove(0, password_key.Length)) : data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(answer);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+//                        Console.WriteLine("Sent: {0}", answer);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+
+        static string CheckPassowrd(string receivedPassword)
+        {
+            for (int i = 0; i < passsword.Length; i++)
+            {
+                if (passsword[i] != receivedPassword[i])
+                    return "False";
+                Thread.Sleep(5);
+            }
+            return "True";
+        }
+    }
+}
