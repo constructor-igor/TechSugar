be9c56d31efa1c8477635fae70c74759839687b0
constructor-igor <igor.zc@gmail.com>
2017-06-11 18:01:14 +0300

    #338 copied task from https://www.coursera.org/learn/scala-spark-big-data/programming/QcWcs/wikipedia

---
+++ ScalaSamples/scala-spark-big-data/week1problem/solution/wikipedia/src/main/scala/main.scala
@@ -0,0 +1,11 @@
+import wikipedia.WikipediaRanking
+
+/**
+  * Created by igor-z on 11-Jun-17.
+  */
+
+object Main extends App {
+    println("Hello, wikipedia sample")
+    WikipediaRanking.main(new Array[String](0))
+    println("Wikipedia sample completed.")
+}
