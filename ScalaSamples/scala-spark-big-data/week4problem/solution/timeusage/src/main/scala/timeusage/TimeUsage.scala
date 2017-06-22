a55a1b99fdfc6a5e4fcd553e230ccaded18fd262
constructor-igor <igor.zc@gmail.com>
2017-06-22 18:17:44 +0300

    #338 implemented classifiedColumns()

--- ScalaSamples/scala-spark-big-data/week4problem/solution/timeusage/src/main/scala/timeusage/TimeUsage.scala
+++ ScalaSamples/scala-spark-big-data/week4problem/solution/timeusage/src/main/scala/timeusage/TimeUsage.scala
@@ -91,7 +91,33 @@ object TimeUsage {
     *    “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
     */
   def classifiedColumns(columnNames: List[String]): (List[Column], List[Column], List[Column]) = {
-    ???
+      val categoryMappings = List(
+          List("t01", "t03", "t11", "t1801", "t1803"),
+          List("t05", "t1805"),
+          List("t02", "t04", "t06", "t07", "t08", "t09", "t10", "t12", "t13", "t14", "t15", "t16", "t18")
+      ).zipWithIndex
+
+      val groups: Map[Int, List[Column]] = columnNames
+          .foldLeft(List.empty[(Int, Column)])((acc, name) => {
+              categoryMappings
+                  .flatMap {
+                      case (prefixes, index) if prefixes.exists(name.startsWith) => Some((index, new Column(name)))
+                      case _ => None
+                  }
+                  .sortBy(_._1)
+                  .headOption match {
+                  case Some(tuple) => tuple :: acc
+                  case None => acc
+              }
+          }
+          )
+          .groupBy(_._1)
+          .mapValues(_.map(_._2))
+
+
+      val results = (0 to 2).map(index => groups.getOrElse(index, List.empty[Column]))
+
+      (results(0), results(1), results(2))
   }
 
   /** @return a projection of the initial DataFrame such that all columns containing hours spent on primary needs
