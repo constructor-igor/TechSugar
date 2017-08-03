2e879966a5e1ce16bda95315753672855071c948
constructor-igor <igor.zc@gmail.com>
2017-08-04 01:40:47 +0300

    #355 added partial working method def convert_to_flat_by_sparkpy

--- pythonSamples/pysparkSamples/df_flat.py
+++ pythonSamples/pysparkSamples/df_flat.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 import findspark
 findspark.init()
@@ -9,7 +9,9 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql import SQLContext, Row
 from pyspark.sql.types import FloatType
-from pyspark.sql import functions
+# from pyspark.sql import 
+from pyspark.sql.functions import first, col
+from pyspark.ml.feature import VectorAssembler
 
 
 def convert_to_flat_by_pandas(df):
@@ -24,16 +26,20 @@ def convert_to_flat_by_pandas(df):
         parameter_values = key_rows['parameter']
         parameter_value = parameter_values.real[0]        
 
-        # key_reference_value = []
-        # for reference_values in key_rows['reference']:
-        #     key_reference_value.append(reference_values)
         key_reference_value = [reference_values for reference_values in key_rows['reference']]
 
         flat_values.append((parameter_value, key_reference_value))
 
-    loaded_data = [(label, Vectors.dense(features)) for (label, features) in flat_values]
+    loaded_data = [(np.asscalar(label), Vectors.dense(features)) for (label, features) in flat_values]
     spark_df = spark.createDataFrame(loaded_data, ["label", "features"])
+    return spark_df
 
+def convert_to_flat_by_sparkpy(df):
+    spark_df = assembler.transform(df.groupBy("key", "parameter").pivot("subkey").agg(first(col("reference"))))    
+    assembler = VectorAssembler().setInputCols(["subkey1", "subkey2"]).setOutputCol("features")
+    spark_df = spark_df.withColumnRenamed("parameter", "label")
+    spark_df = spark_df.select("label", "features")
+    # spark_df = spark_df.select(spark_df.label, Vectors.dense(spark_df.features))
     return spark_df
 
 if __name__ == "__main__":
@@ -46,10 +52,12 @@ if __name__ == "__main__":
     print("spark version:", spark.version)
 
     data = [
-        {'key': 'key1', 'subkey': 'subkey1', 'parameter': '45', 'reference': '10'},
-        {'key': 'key1', 'subkey': 'subkey2', 'parameter': '45', 'reference': '20'},
-        {'key': 'key2', 'subkey': 'subkey2', 'parameter': '70', 'reference': '40'},
-        {'key': 'key2', 'subkey': 'subkey1', 'parameter': '70', 'reference': '30'}
+        {'key': 'key1', 'subkey': 'subkey1', 'parameter': 45.0, 'reference': 10.0},
+        {'key': 'key1', 'subkey': 'subkey3', 'parameter': 45.0, 'reference': 20.0},
+        {'key': 'key1', 'subkey': 'subkey2', 'parameter': 45.0, 'reference': 15.0},
+        {'key': 'key2', 'subkey': 'subkey2', 'parameter': 70.0, 'reference': 40.0},
+        {'key': 'key2', 'subkey': 'subkey3', 'parameter': 70.0, 'reference': 50.0},
+        {'key': 'key2', 'subkey': 'subkey1', 'parameter': 70.0, 'reference': 30.0}
     ]
     
     original_df = spark.createDataFrame(data)
@@ -57,7 +65,15 @@ if __name__ == "__main__":
     original_df.show()
 
     flat_df = convert_to_flat_by_pandas(original_df)
+    print("schema of result's data frame:")
+    flat_df.printSchema()
     print('result data (by pandas):')
-    flat_df.show()
+    flat_df.show()    
+
+    flat_df = convert_to_flat_by_sparkpy(original_df)
+    print("schema of result's data frame:")
+    flat_df.printSchema()
+    print('result data (by sparkby):')
+    flat_df.show()    
 
     print("main completed")
