7b1a9939638c688155180be9b2f089e5115e64b2
constructor-igor <igor.zc@gmail.com>
2017-08-03 11:16:26 +0300

    #354 fixed sample

--- AutoMapper/AutoMapper/Program.cs
+++ AutoMapper/AutoMapper/Program.cs
@@ -6,16 +6,18 @@ namespace AutoMapperConsole
     public class ClientOrder
     {
         public string Name { get; }
+        public string Id { get; }
 
-        public ClientOrder(string name)
+        public ClientOrder(string name, string id)
         {
             Name = name;
+            Id = id;
         }
 
         #region Overrides of Object
         public override string ToString()
         {
-            return $"Name={Name}";
+            return $"[ClientOrder] Name={Name}, Id={Id}";
         }
         #endregion
     }
@@ -23,15 +25,17 @@ namespace AutoMapperConsole
     public class EngineOrder
     {
         public string Name { get; }
+        public string Id { get; }
 
-        public EngineOrder(string name)
+        public EngineOrder(string name, string id)
         {
             Name = name;
+            Id = id;
         }
         #region Overrides of Object
         public override string ToString()
         {
-            return $"Name={Name}";
+            return $"[EngineOrder] Name={Name}, Id={Id}";
         }
         #endregion
     }
@@ -40,16 +44,21 @@ namespace AutoMapperConsole
     {
         static void Main(string[] args)
         {
-            Mapper.Initialize(cfg => cfg.CreateMap<ClientOrder, EngineOrder>());
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<ClientOrder, EngineOrder>();
+                cfg.CreateMap<EngineOrder, ClientOrder>();
+            });
             // or var config = new MapperConfiguration(cfg => cfg.CreateMap<Order, OrderDto>());
 
-            ClientOrder clientOrder = new ClientOrder("Joe");
+            ClientOrder clientOrder = new ClientOrder("Joe", "1s0");
 
             IMapper mapper = Mapper.Configuration.CreateMapper();
-            var engineOrder = mapper.Map<ClientOrder>(clientOrder);
+            var engineOrder = mapper.Map<EngineOrder>(clientOrder);
 
-            Console.WriteLine("client order: {0}", clientOrder);
-            Console.WriteLine("engine order: {0}", engineOrder);
+            Console.WriteLine("client order (original order) : {0}", clientOrder);
+            Console.WriteLine("engine order (by client order): {0}", engineOrder);
+            Console.WriteLine("client order (by engine order): {0}", mapper.Map<ClientOrder>(engineOrder));
         }
     }
 }
