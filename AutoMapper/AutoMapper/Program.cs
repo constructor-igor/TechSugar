1395702a263e326249eaa5274d8620cfd1e02ac4
constructor-igor <igor.zc@gmail.com>
2017-08-03 11:08:00 +0300

    #354 created first sample

--- AutoMapper/AutoMapper/Program.cs
+++ AutoMapper/AutoMapper/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AutoMapper;
 
-namespace AutoMapper
+namespace AutoMapperConsole
 {
+    public class ClientOrder
+    {
+        public string Name { get; }
+
+        public ClientOrder(string name)
+        {
+            Name = name;
+        }
+
+        #region Overrides of Object
+        public override string ToString()
+        {
+            return $"Name={Name}";
+        }
+        #endregion
+    }
+
+    public class EngineOrder
+    {
+        public string Name { get; }
+
+        public EngineOrder(string name)
+        {
+            Name = name;
+        }
+        #region Overrides of Object
+        public override string ToString()
+        {
+            return $"Name={Name}";
+        }
+        #endregion
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Mapper.Initialize(cfg => cfg.CreateMap<ClientOrder, EngineOrder>());
+            // or var config = new MapperConfiguration(cfg => cfg.CreateMap<Order, OrderDto>());
+
+            ClientOrder clientOrder = new ClientOrder("Joe");
+
+            IMapper mapper = Mapper.Configuration.CreateMapper();
+            var engineOrder = mapper.Map<ClientOrder>(clientOrder);
+
+            Console.WriteLine("client order: {0}", clientOrder);
+            Console.WriteLine("engine order: {0}", engineOrder);
         }
     }
 }
