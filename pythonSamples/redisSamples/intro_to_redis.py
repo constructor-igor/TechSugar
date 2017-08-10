3cbaced98124786ce38b266cdaaf9b207ce60d04
constructor-igor <igor.zc@gmail.com>
2017-08-10 17:28:39 +0300

    #356 add first sample

---
+++ pythonSamples/redisSamples/intro_to_redis.py
@@ -0,0 +1,31 @@
+# 
+# https://github.com/andymccurdy/redis-py
+# 
+import configparser
+import redis
+
+def read_host_name():
+    config = configparser.ConfigParser()
+    config['General'] = {'host': 'localhost'}
+    config.read('user.ini')
+    host = config['General']['host']
+    return host
+
+def connect_to_redis(host):
+    redis_connection = redis.StrictRedis(host=host, port=6379, db=0)
+    return redis_connection
+
+if __name__ == "__main__":
+    print("main started")  
+    host = read_host_name()
+    print('host:', host)
+
+    redis_connection = connect_to_redis(host)
+
+    result = redis_connection.set('igor:simple', 'intro')
+    print('set: ', result)
+
+    result = redis_connection.get('igor:simple')
+    print('get: ', result)
+
+    print("main completed")
