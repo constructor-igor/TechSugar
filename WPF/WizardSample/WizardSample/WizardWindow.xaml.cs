83f712db41e4e3b852a4254dec958344a2c44374
constructor-igor <igor.zc@gmail.com>
2017-10-18 13:52:26 +0300

    #365 created wizard sample

---
+++ WPF/WizardSample/WizardSample/WizardWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WizardSample
+{
+    /// <summary>
+    /// Interaction logic for WizardWindow.xaml
+    /// </summary>
+    public partial class WizardWindow : Window
+    {
+        public WizardWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FirstNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Page1.CanSelectNextPage = !string.IsNullOrEmpty(FirstNameTextBox.Text);
+        }
+
+        private void LastNameTextBox_OnTextChangedNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Page1.CanSelectNextPage = !string.IsNullOrEmpty(LastNameTextBox.Text);
+        }
+    }
+}
