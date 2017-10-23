1c8fb6a8e3a4bf675b5dc52b639eb91061cf38b0
constructor-igor <igor.zc@gmail.com>
2017-10-23 17:59:48 +0300

    #364 added sew+nlog integration sample

---
+++ LogsLab/SeqNLogSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using NLog;
+
+namespace SeqNLogSample
+{
+    class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            foreach (int index in Enumerable.Range(0, 100))
+            {
+                Logger.Info($"Hello, {index}!");
+            }            
+        }
+    }
+}
