eba592f421b7ebc6a48f97c4dc6c050a7d7faa6c
constructor-igor <igor.zc@gmail.com>
2017-10-23 17:32:58 +0300

    #364 created SeqSample project

---
+++ LogsLab/SeqSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Serilog;
+
+namespace SeqSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger();
+
+            Log.Information("Hello, {Name}!", Environment.UserName);
+
+            // Important to call at exit so that batched events are flushed.
+            Log.CloseAndFlush();
+
+            Console.ReadKey(true);
+        }
+    }
+}
