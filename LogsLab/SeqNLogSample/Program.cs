ced99bf794750ebabcd4929f8b50f84c82446c8c
constructor-igor <igor.zc@gmail.com>
2017-10-24 12:21:04 +0300

    #364 added different levels of messages

--- LogsLab/SeqNLogSample/Program.cs
+++ LogsLab/SeqNLogSample/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using NLog;
 
 namespace SeqNLogSample
@@ -8,10 +9,17 @@ namespace SeqNLogSample
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            Logger.Info($"started, {DateTime.Now}");
             foreach (int index in Enumerable.Range(0, 100))
             {
                 Logger.Info($"Hello, {index}!");
-            }            
+                Logger.Warn("warning");
+                if (index == 90)
+                    Logger.Error("error");
+                if (index == 95)
+                    Logger.Fatal("fatal");
+            }
+            Logger.Info($"finished, {DateTime.Now}");
         }
     }
 }
