354613afa7d1b183928de6903ec16769512418dd
constructor-igor <igor.zc@gmail.com>
2017-12-26 10:12:44 +0200

    updated dict_sample.py

--- pythonSamples/tips/dict_sample.py
+++ pythonSamples/tips/dict_sample.py
@@ -1,17 +1,19 @@
 import copy
 
 
-def update_dic(d):
+def update_dict(d):
     d["test"] = "2"
-    return d
+    return d            # can be removed from the function
 
 if __name__ == "__main__":
     d = dict()
     d["test"] = "1"
-    print("before update: ", d)
-    updated_d = update_dic(d)
-    print("after update: ", d)
+    print("[d] before update: ", d)
+    updated_d = update_dict(d)
+    print("[d] after update: ", d)
+    print("[updated_d]: ", d)
     new_d = copy.deepcopy(d)
+    print("[new d]: ", new_d)
 
     print("if d is update_d: ", d is updated_d)
     print("if d is new_d: ", d is new_d)
