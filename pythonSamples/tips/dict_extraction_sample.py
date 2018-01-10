085d1f66f0cc0dd330e1d19a06710fa14b3d0668
constructor-igor <igor.zc@gmail.com>
2018-01-10 16:09:21 +0200

    added dict_extraction_sample.py

---
+++ pythonSamples/tips/dict_extraction_sample.py
@@ -0,0 +1,18 @@
+
+
+big_dict = {'a':1,'b':2, 'c':3, 'z':26} 
+
+# 
+# straightforward way
+# 
+short_keys = ['a', 'z']
+short_dict = dict()
+for key in short_keys:
+    short_dict[key] = big_dict[key]
+print(short_dict)
+
+# 
+# short way
+# 
+short_dict = dict((key, big_dict[key]) for key in short_keys if key in big_dict)
+print(short_dict)
