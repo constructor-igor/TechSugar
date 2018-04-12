3e999778b7b9205006c81932345630dedd201aaa
constructor-igor <igor.zc@gmail.com>
2018-04-12 17:30:24 +0300

    added struct array/list (mutable) test

---
+++ csharp-tips/csharp-tips/csharp-tips/MutableSample.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace csharp_tips
+{
+    public struct Mutable
+    {
+        private int _x;
+        public Mutable(int x)
+        {
+            _x = x;
+        }
+ 
+        public int X => _x;
+
+        public void IncrementX() { _x++; }
+    }
+
+    [TestFixture]
+    public class MutableTests
+    {
+        [Test]
+        public void CheckMutability()
+        {
+            var ma = new[] {new Mutable(1)};
+            ma[0].IncrementX();
+            // X has been changed!
+            Assert.That(ma[0].X, Is.EqualTo(2));
+
+            var ml = new List<Mutable> {new Mutable(1)};
+            ml[0].IncrementX();
+            // X hasn't been changed!
+            Assert.That(ml[0].X, Is.EqualTo(1));
+        }
+    }
+}
