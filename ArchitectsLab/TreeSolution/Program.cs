d29e83ca4e6983aa00a85a53617ecc1548125c7e
constructor-igor <igor.zc@gmail.com>
2018-04-12 09:09:48 +0300

    added several c# tips

--- ArchitectsLab/TreeSolution/Program.cs
+++ ArchitectsLab/TreeSolution/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace TreeSolution
 {
@@ -11,6 +12,40 @@ namespace TreeSolution
             tree.PrintTreeWithRecursion();
             tree.PrintTreeWithoutRecursion();
             tree.PrintTreeWithoutRecursion_Stack(node=>Console.WriteLine(node.ID));
+
+            StringBuilder sb = new StringBuilder()
+                .Append("Text")
+                .AppendLine("tree")
+                .Replace("tr", "rr");
+
+            MyStringBuilder myStringBuilder = new MyStringBuilder();
+            myStringBuilder
+                .Add("1")
+                .Add("2");
+
+            myStringBuilder
+                .Add2("t1", "t2")
+                .Add2("t1", "t2");
+        }
+    }
+    public class MyStringBuilder
+    {
+        private String fullText = "";
+        public MyStringBuilder Add(string text)
+        {
+            fullText = fullText + text;
+            return this;
+        }
+    }
+
+    public static class ExtMethods
+    {
+        public static MyStringBuilder Add2(this MyStringBuilder sb, string t1, string t2)
+        {
+            sb
+                .Add(t1)
+                .Add(t2);
+            return sb;
         }
     }
 }
--- csharp-tips/csharp-tips/csharp-tips/ReferenceTests.cs
+++ csharp-tips/csharp-tips/csharp-tips/ReferenceTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace csharp_tips
@@ -5,6 +6,30 @@ namespace csharp_tips
     [TestFixture]
     public class ReferenceTests
     {
+        public class Point
+        {
+            public static Point Init = new Point();
+            public double X;
+            public double Y;
+
+            public void PrintInit()
+            {
+                Console.WriteLine("X={0}, Init = ({1}, {2})", X, Init.X, Init.Y);
+                PrintStatic();
+            }
+
+            private static void PrintStatic()
+            {
+                Console.WriteLine("Init = ({0}, {1})", Init.X, Init.Y);
+            }
+        }
+
+        public struct PointStruct
+        {
+            public int X;
+            public int Y;
+        }
+
         [Test]
         public void Test()
         {
@@ -15,6 +40,131 @@ namespace csharp_tips
 
             function2(ref objectA);
             Assert.That(objectA, Is.Null);
+        }
+
+        [Test]
+        public void PointTest()
+        {
+            Point p = new Point() {X = 1, Y = 2};
+            Foo(p);
+            Assert.That(p.X, Is.EqualTo(1));
+            Foo2(p);
+            Assert.That(p.X, Is.EqualTo(9));
+
+            Point one = new Point() {X = 100};
+            Point two = new Point() {X = 200};
+            Point.Init.X = 10;
+            one.PrintInit();
+            two.PrintInit();
+            Point.Init.X = 20;
+            one.PrintInit();
+            two.PrintInit();
+        }
+        [Test]
+        public void PointTestWithRef()
+        {
+            Point p = new Point { X = 1, Y = 2 };
+            FooRef(ref p);
+            Assert.That(p.X, Is.EqualTo(10));
+        }
+        [Test]
+        public void PointTestWithOut()
+        {
+            Point p = new Point { X = 1, Y = 2 };
+            FooOut(out p);
+            Assert.That(p.X, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void PointStructTest()
+        {
+            PointStruct p = new PointStruct() { X = 1, Y = 2 };
+            FooStruct(p);
+            Assert.That(p.X, Is.EqualTo(1));
+            FooRefStruct(ref p);
+            Assert.That(p.X, Is.EqualTo(100));
+            FooStruct2(p);
+            Assert.That(p.X, Is.EqualTo(100));
+        }
+
+        void FooStruct(PointStruct pstruct)
+        {
+            pstruct = new PointStruct() {X=10, Y=20};
+        }
+        void FooRefStruct(ref PointStruct pstruct)
+        {
+            pstruct = new PointStruct() { X = 100, Y = 200 };
+        }
+        void FooStruct2(PointStruct pstruct)
+        {
+            pstruct.X = 5;
+        }
+
+        [Test]
+        public void TestString()
+        {
+            string line = "12345";
+            line = line + "6";
+            FooString(line);
+            Assert.That(line, Is.EqualTo("123456"));
+            FooRefString(ref line);
+            Assert.That(line, Is.EqualTo("abc"));
+        }
+
+        [Test]
+        public void TestInt()
+        {
+            int number = 12345;
+            FooNumber(number);
+            Assert.That(number, Is.EqualTo(12345));
+            FooRefNumber(ref number);
+            Assert.That(number, Is.EqualTo(10));
+        }
+
+        public void FooNumber(int pnumber)
+        {
+            pnumber = 10;
+        }
+        public void FooRefNumber(ref int pnumber)
+        {
+            pnumber = 10;
+        }
+
+        static string line = "12345";
+        [Test]        
+        public void TestStaticString()
+        {            
+            line = line + "6";
+            FooString(line);
+            Assert.That(line, Is.EqualTo("123456"));
+            FooRefString(ref line);
+            Assert.That(line, Is.EqualTo("abcd"));
+        }
+
+        void FooString(string pline)
+        {
+            pline = "abc";
+        }
+        void FooRefString(ref string pline)
+        {
+            pline = "abcd";
+        }
+
+        void Foo(Point p)
+        {
+            p = new Point() {X = 10, Y = 20};
+        }
+        void Foo2(Point p)
+        {
+            p.X = 9;
+        }
+        void FooRef(ref Point p)
+        {
+            p = new Point() { X = 10, Y = 20 };
+        }
+        void FooOut(out Point p)
+        {
+            p = new Point() { X = 10, Y = 20 };
         }
 
         void function1(object objectX)
