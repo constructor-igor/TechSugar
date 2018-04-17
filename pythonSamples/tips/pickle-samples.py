8b3cfe9df63ff721850a6ab2737767a010b46416
constructor-igor <igor.zc@gmail.com>
2018-04-17 19:26:07 +0300

    added pickale samples

--- pythonSamples/tips/pickle-samples.py
+++ pythonSamples/tips/pickle-samples.py
@@ -1,5 +1,13 @@
 import pickle
 
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    def print_status(self):
+        print("[Animal] name: {0}".format(self.name))
+    @staticmethod
+    def print_status_static(name):
+        print("[Animal(static)] name: {0}".format(name))
 
 original_object = { "abc" : [1, 2, 3], "qwerty" : [4, 5, 6, 7] }
 with open('d.pkl', 'wb') as file_handle_write:
@@ -12,3 +20,17 @@ with open('d.pkl', 'rb') as file_handle_read:
 
 #print object loaded from file
 print('loaded_object: ', loaded_object)
+
+dog = Animal("dog")
+dog.print_status()
+
+animal_serialized = pickle.dumps(dog)
+animal_restored = pickle.loads(animal_serialized)
+animal_restored.print_status()
+
+method = Animal.print_status_static
+method("cat")
+
+method_serialized = pickle.dumps(method)
+method_restored = pickle.loads(method_serialized)
+method_restored("camel")
