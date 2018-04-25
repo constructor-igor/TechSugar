a7c215788a989133975c3093d9718953b288a275
constructor-igor <igor.zc@gmail.com>
2018-04-25 16:19:47 +0300

    #392 added support of local path

--- csharp-tips/ClickOnceSample/ClickOnceSample/Form1.Designer.cs
+++ csharp-tips/ClickOnceSample/ClickOnceSample/Form1.Designer.cs
@@ -36,7 +36,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Name = "Form1";
-            this.Text = "Click Once Sample b2";
+            this.Text = "Click Once Sample b10";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- csharp-tips/ClickOnceSample/ClickOnceSample/Form1.cs
+++ csharp-tips/ClickOnceSample/ClickOnceSample/Form1.cs
@@ -16,5 +16,10 @@ namespace ClickOnceSample
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
