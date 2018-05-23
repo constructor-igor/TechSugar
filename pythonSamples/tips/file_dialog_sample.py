b4e12259e79d6ee9cd3af0b55b1fcb68901b5fa1
constructor-igor <igor.zc@gmail.com>
2018-05-23 09:57:56 +0300

    #398

--- pythonSamples/tips/file_dialog_sample.py
+++ pythonSamples/tips/file_dialog_sample.py
@@ -28,6 +28,11 @@ def win_dialog_thread(arg):
     folder_dialog = FolderBrowserDialog()
     file_dialog = OpenFileDialog()
 
+    title = arg.Dequeue()
+    initial_folder = arg.Dequeue()
+    print(f"title: {title}, initial_folder: {initial_folder}")
+    folder_dialog.Description = title
+    folder_dialog.SelectedPath = initial_folder
     folder_dialog_result = folder_dialog.ShowDialog()
     file_dialog_result = file_dialog.ShowDialog()
 
@@ -35,12 +40,53 @@ def win_dialog_thread(arg):
     selected_file = file_dialog.FileName
     win_dialog_result = selected_folder, selected_file
     arg.Enqueue(win_dialog_result)
-    print(f"dialog_thread completed, {win_dialog_result}")
+    print(f"win_dialog_thread completed, {win_dialog_result}")
 
-def open_win_dialog():
+def open_win_dialog(title, initial_folder):
     #   start dialogs via CLR
-    out_queue = Queue()    
+    out_queue = Queue()
+    out_queue.Enqueue(title)
+    out_queue.Enqueue(initial_folder)
     start = ParameterizedThreadStart(win_dialog_thread)
+    thread = Thread(start)
+    thread.SetApartmentState(ApartmentState.STA)
+    thread.Start(out_queue)
+    thread.Join()
+    final_result = out_queue.Dequeue()
+    return final_result[0], final_result[1]
+
+
+def tkinter_dialog_thread(arg):
+    print(f"dialog_thread started")
+    title = arg.Dequeue()
+    initial_folder = arg.Dequeue()
+
+    root = tk.Tk()
+    root.withdraw()
+    selected_folder = askdirectory(title=title, initialdir=initial_folder)
+    selected_file = ""
+
+    # folder_dialog = FolderBrowserDialog()
+    # file_dialog = OpenFileDialog()
+
+    # print(f"title: {title}, initial_folder: {initial_folder}")
+    # folder_dialog.Description = title
+    # folder_dialog.SelectedPath = initial_folder
+    # folder_dialog_result = folder_dialog.ShowDialog()
+    # file_dialog_result = file_dialog.ShowDialog()
+
+    # selected_folder = folder_dialog.SelectedPath
+    # selected_file = file_dialog.FileName
+    win_dialog_result = selected_folder, selected_file
+    arg.Enqueue(win_dialog_result)
+    print(f"tkinter_dialog_thread completed, {win_dialog_result}")
+
+
+def open_tkinter_dialog(title, initial_folder):
+    out_queue = Queue()
+    out_queue.Enqueue(title)
+    out_queue.Enqueue(initial_folder)
+    start = ParameterizedThreadStart(tkinter_dialog_thread)
     thread = Thread(start)
     thread.SetApartmentState(ApartmentState.STA)
     thread.Start(out_queue)
@@ -55,7 +101,10 @@ if __name__ == "__main__":
     # the method stuck when 'import clr' works before 
     # open_dialog()
 
-    result = open_win_dialog()
+    result = open_win_dialog("select folder", "d:\\")
     print(f"open_win_dialog: {result}")
+
+    result = open_tkinter_dialog("select folder", "d:\\")
+    print(f"open_tkinter_dialog: {result}")
 
     print("main finished")
