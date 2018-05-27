1dc636ae03be4a627df21a2578c662e2aa22a0cb
constructor-igor <igor.zc@gmail.com>
2018-05-27 12:14:35 +0300

    #399 NNSharp (Keras) sample

---
+++ ML/NNSharpSamples/NNSharpConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using NNSharp.DataTypes;
+using NNSharp.IO;
+using NNSharp.Models;
+
+namespace NNSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kerasModelFile = "";
+            Sample1(kerasModelFile);
+        }
+
+        public static void Sample1(string filePath)
+        {
+            // Read the previously created json.
+            var reader = new ReaderKerasModel(filePath);
+            SequentialModel model = reader.GetSequentialExecutor();
+
+            // Then create the data to run the executer on.
+            // batch: should be set in the Keras model.
+            int height = 0;
+            int width = 0;
+            int channel = 0;
+            int batch = 0;
+            Data2D input = new Data2D(height, width, channel, batch);
+
+            // Calculate the network's output.
+            IData output = model.ExecuteNetwork(input);
+        }
+    }
+}
