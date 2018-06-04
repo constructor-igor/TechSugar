f84dc99ccd57aea0b7788afcd3c2699396ae0dc2
constructor-igor <igor.zc@gmail.com>
2018-06-04 10:25:05 +0300

    #400: added sample with "Python.Runtime.dll  3.6"

---
+++ pythonnet/pythonnetsamples/samples/DemoTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using Python.Runtime;
+
+namespace samples
+{
+    [TestFixture]
+    public class DemoTests
+    {
+        [Test]
+        public void RunNumPy()
+        {
+            using (Py.GIL())
+            {
+                dynamic np = Py.Import("numpy");
+                dynamic sin = np.sin;
+                double value = sin(Math.PI / 2);
+                Assert.That(value, Is.EqualTo(1.00));
+            }
+        }
+
+        [Test]
+        public void RunCustomMethod()
+        {
+            string moduleDirectory = GetDllLocation();
+            using (Py.GIL())
+            {
+                dynamic syspy = Py.Import("sys");
+                syspy.path.append(moduleDirectory);
+
+                dynamic customModule = Py.Import("custom");
+                dynamic run = customModule.run;
+                string answer = run();
+                Assert.That(answer, Is.EqualTo("Hello from python"));
+            }
+        }
+
+        private string GetDllLocation()
+        {
+            string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return assemblyFolder;
+        }
+    }
+}
---
+++ pythonnet/pythonnetsamples/samples/custom.py
@@ -0,0 +1,3 @@
+﻿
+def run():
+    return "Hello from python"
