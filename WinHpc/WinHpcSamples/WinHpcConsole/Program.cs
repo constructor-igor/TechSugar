fdf417211d9d0689eb6236495d3c1caca9c141e2
constructor-igor <igor.zc@gmail.com>
2018-08-05 12:55:33 +0300

    added WinHpc sample

---
+++ WinHpc/WinHpcSamples/WinHpcConsole/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Hpc.Scheduler;
+using Microsoft.Hpc.Scheduler.Properties;
+
+namespace WinHpcConsole
+{
+    class Program
+    {
+        private static readonly ManualResetEvent manualEvent = new ManualResetEvent(false);
+        static void Main(string[] args)
+        {
+            Console.Write("hpc: ");
+            string serverName = Console.ReadLine();
+            Console.Write("user: ");
+            string user = Console.ReadLine();
+
+            IScheduler scheduler = new Scheduler();
+
+            scheduler.Connect(serverName);
+            ISchedulerJob job = scheduler.CreateJob();
+            ISchedulerTask task = job.CreateTask();
+            task.CommandLine = "dir";
+            job.AddTask(task);
+
+            // Specify the events that you want to receive.
+            job.OnJobState += JobStateCallback;
+            job.OnTaskState += TaskStateCallback;
+
+            // Start the job.
+            scheduler.SubmitJob(job, user, null);
+            manualEvent.WaitOne();
+        }
+
+        private static void TaskStateCallback(object sender, TaskStateEventArg e)
+        {
+        }
+
+        private static void JobStateCallback(object sender, JobStateEventArg e)
+        {
+            string newState = e.NewState.ToString();
+            Console.WriteLine($"{e.JobId}: {newState}");
+
+            if (JobState.Canceled == e.NewState ||
+                JobState.Failed == e.NewState ||
+                JobState.Finished == e.NewState)
+            {
+                manualEvent.Set();
+            }
+        }
+    }
+}
