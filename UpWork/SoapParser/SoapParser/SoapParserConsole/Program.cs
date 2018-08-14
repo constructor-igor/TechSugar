c0e91d45b2fa94469a7046980a790ac08e2685e1
constructor-igor <igor.zc@gmail.com>
2018-08-14 23:14:25 +0300

    #414 implemented soap parser

---
+++ UpWork/SoapParser/SoapParser/SoapParserConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace SoapParserConsole
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string fileName = args.Length > 0 ? args[1] : @"data\soapExample2.txt";
+            bool fileExists = File.Exists(fileName);
+            Console.WriteLine($"File {fileName} exists: {fileExists}");
+
+            if (!fileExists)
+            {
+                Console.WriteLine($"Exit, because file {fileName} not found.");
+                return -1;
+            }
+
+            SoapParser parser = new SoapParser(fileName);
+            string sessionId = parser.GetSessionID();
+            Console.WriteLine($"Found value of 'SessionID' is {sessionId}");
+            return 0;
+        }
+    }
+}
---
+++ UpWork/SoapParser/SoapParser/SoapParserConsole/SoapParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SoapParserConsole
+{
+    public class SoapParser
+    {
+        private readonly XElement m_soapFromFile;
+        public SoapParser(string fileName)
+        {
+            m_soapFromFile = XElement.Load(fileName);
+        }
+
+        public void Print()
+        {
+            PrintElemets(".", m_soapFromFile);
+        }
+
+        public string GetSessionID()
+        {
+            return Find_pcSessionID(m_soapFromFile);
+        }
+
+        private void PrintElemets(string prefix, XElement node)
+        {
+            foreach (XElement element in node.Elements())
+            {
+                Console.WriteLine(prefix + element.Name);
+                foreach (XElement elementI in element.Elements())
+                {
+                    PrintElemets(prefix + ".", elementI);
+                }
+            }
+        }
+
+        private string Find_pcSessionID(XElement node)
+        {
+            foreach (XElement element in node.Elements())
+            {
+                if (element.Name.LocalName == "contextName" && element.Value == "pcSessionID")
+                {
+                    List<XElement> allParentElements = element.Parent.Elements().ToList();
+                    string foundValue = allParentElements.Find(e => e.Name.LocalName == "contextValue").Value;
+                    return foundValue;
+                }
+                foreach (XElement elementI in element.Elements())
+                {
+                    string value = Find_pcSessionID(elementI);
+                    if (value != null)
+                        return value;
+                }
+            }
+
+            return null;
+        }
+    }
+}
