be5ee27a7260db305a70a6b9cb69431144fd32f7
constructor-igor <igor.zc@gmail.com>
2018-09-11 22:18:54 +0300

    added c# samples

---
+++ csharp-tips/csharp-tips/csharp-tips/DeadLockSample.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace csharp_tips
+{
+    [TestFixture]
+    public class DeadLockSampleTests
+    {
+        [Test]
+        public void Test()
+        {
+            DeadLockSample.Run();
+        }
+    }
+
+    public class DeadLockSample
+    {
+        static readonly object firstLock = new object();
+        static readonly object secondLock = new object();
+        static void ThreadJob()
+        {
+            Console.WriteLine("\t\t\t\tLocking firstLock");
+            lock (firstLock)
+            {
+                Console.WriteLine("\t\t\t\tLocked firstLock");
+                // Wait until we're fairly sure the first thread
+                // has grabbed secondLock
+                Thread.Sleep(1000);
+                Console.WriteLine("\t\t\t\tLocking secondLock");
+                lock (secondLock)
+                {
+                    Console.WriteLine("\t\t\t\tLocked secondLock");
+                }
+                Console.WriteLine("\t\t\t\tReleased secondLock");
+            }
+            Console.WriteLine("\t\t\t\tReleased firstLock");
+        }
+        public static void Run()
+        {
+            new Thread(new ThreadStart(ThreadJob)).Start();
+            // Wait until we're fairly sure the other thread
+            // has grabbed firstLock
+            Thread.Sleep(500);
+            Console.WriteLine("Locking secondLock");
+            lock (secondLock)
+            {
+                Console.WriteLine("Locked secondLock");
+                Console.WriteLine("Locking firstLock");
+                lock (firstLock)
+                {
+                    Console.WriteLine("Locked firstLock");
+                }
+                Console.WriteLine("Released firstLock");
+            }
+            Console.WriteLine("Released secondLock");
+            Console.Read();
+        }
+    }
+}
--- csharp-tips/csharp-tips/csharp-tips/LINQ/TakeSample.cs
+++ csharp-tips/csharp-tips/csharp-tips/LINQ/TakeSample.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace csharp_tips.LINQ
@@ -19,6 +21,24 @@ namespace csharp_tips.LINQ
             int[] data = Enumerable.Range(0, 10).ToArray();
             int[] part = data.Take(15).ToArray();
             Assert.That(part.Length, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void TestSelect()
+        {
+            Console.WriteLine("start");
+            List<int> data = new List<int>{0, 2, 4, 5};
+            var query = data.Select(item =>
+            {
+                Console.WriteLine($"item: {item}");
+                return item * 2;
+            });
+            Console.WriteLine("after select");
+            data.Add(10);
+            int[] result = query.ToArray();
+            Console.WriteLine(query.Count());
+            Console.WriteLine($"result: {string.Join(",", result)}");
+            Console.WriteLine("finish");
         }
     }
 }
