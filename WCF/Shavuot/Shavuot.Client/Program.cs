bba00e1fcdb2e0a41e65e0cf384b74e273cd0513
constructor-igor <igor.zc@gmail.com>
2019-06-07 00:02:16 +0300

    #435 implemented Shavuot.Client

--- WCF/Shavuot/Shavuot.Client/Program.cs
+++ WCF/Shavuot/Shavuot.Client/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ServiceModel;
+using Shavuot.Contract;
 
 namespace Shavuot.Client
 {
@@ -7,6 +9,17 @@ namespace Shavuot.Client
         static void Main(string[] args)
         {
             Console.WriteLine("[Shavuot.Client] started");
+            ChannelFactory<IShavuotService> cf = new ChannelFactory<IShavuotService>("ShavuotServiceEndpoint");
+            IShavuotService proxy = cf.CreateChannel();
+
+            string message = "";
+            do
+            {
+                Console.Write("Message (<Enter> - finish): ");
+                message = Console.ReadLine();
+                if (message!="")
+                    proxy.Greeting(message);
+            } while (message != "");
         }
     }
 }
