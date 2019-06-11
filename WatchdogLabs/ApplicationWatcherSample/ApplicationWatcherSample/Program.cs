f3e6064f1cd7fba422f2ad966f82b6a046e254bc
constructor-igor <igor.zc@gmail.com>
2019-06-11 15:42:08 +0300

    #436 updated sample 2

--- WatchdogLabs/ApplicationWatcherSample/ApplicationWatcherSample/Program.cs
+++ WatchdogLabs/ApplicationWatcherSample/ApplicationWatcherSample/Program.cs
@@ -5,14 +5,23 @@
  *
  */
 
+using System;
+using System.IO;
+
 namespace ApplicationWatcher.Watchdog
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string workingFolder = @"D:\My\@gh\TechSugar\WatchdogLabs\ApplicationWatcherSample\ApplicationWatcherSample.Client\bin\Debug";
+            string workingFolder = @"..\..\..\ApplicationWatcherSample.Client\bin\Debug";
             string monitoredApplicationName = "ApplicationWatcherSample.Client";
+            string fullPath = Path.GetFullPath(Path.Combine(workingFolder, monitoredApplicationName+".exe"));
+            if (!File.Exists(fullPath))
+            {
+                Console.WriteLine($"Not found monitored application {fullPath}");
+                return;
+            }
             ApplicationWatcher applicationWatcher = new ApplicationWatcher(workingFolder, monitoredApplicationName, "WatchDog", 5000);
         }
     }
