092f963bd12ad1451b224301f05f2888986b289a
constructor-igor <igor.zc@gmail.com>
2019-06-11 15:01:54 +0300

    #436 added empty SeriveSample.Watchdog project

---
+++ WatchdogLabs/ConsoleSample/ServiceSample.Watchdog/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceProcess;
+
+namespace ServiceSample.Watchdog
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun = new ServiceBase[]
+            {
+                new ServiceWatchdog()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
