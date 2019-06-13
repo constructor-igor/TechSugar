e74878efb7fd5b12f19d7f13822881c67da93d65
constructor-igor <igor.zc@gmail.com>
2019-06-13 12:18:38 +0300

    #437 added c# (.NET Core) simple client

---
+++ ElasticSearch/cs_sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Nest;
+
+namespace cs_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string host = @"http://localhost:9200";
+            var node = new Uri(host);
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+            PingResponse pingResponse = client.Ping();
+            // var response = client.ClusterHealth();
+            Console.WriteLine(pingResponse);
+            // Console.Read();
+            Console.WriteLine("finished");
+        }
+    }
+}
