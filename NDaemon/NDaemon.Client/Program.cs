7aa26182bd80daae6172487554d2a68aeb534807
constructor-igor <igor.zc@gmail.com>
2019-06-15 23:48:48 +0300

    #438 created NDaemon

---
+++ NDaemon/NDaemon.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NDaemon.Framework;
+
+namespace NDaemon.Client
+{
+    class Program
+    {
+        private const string AppGuid = "9291DD5E-64C6-4C7F-9FBC-C0CB51F676C1";
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"[NDaemon.Client ({AppGuid})] started.");
+            using (SingleProcess singleProcess = new SingleProcess(AppGuid))
+            {
+                if (singleProcess.OtherProcessRunning)
+                {
+                    Console.WriteLine($"[NDaemon.Client ({AppGuid})] found other process.");
+                    return;
+                }
+                Console.WriteLine($"[NDaemon.Client ({AppGuid})] Single instance of the client started.");
+                Console.Write("To exit from program, press <Enter>.");
+                Console.ReadLine();
+            }
+            Console.WriteLine($"[NDaemon.Client ({AppGuid})] finished.");
+        }
+    }
+}
---
+++ NDaemon/NDaemon.Framework/SingleProcess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace NDaemon.Framework
+{
+    public class SingleProcess: IDisposable
+    {
+        private Mutex m_mutex;
+
+        public bool OtherProcessRunning { get; private set; }
+
+        public SingleProcess(string appGuid)
+        {
+            bool createdNew;
+            m_mutex = new Mutex(true, appGuid, out createdNew);
+            if (createdNew)
+            {
+                m_mutex.ReleaseMutex();
+            }
+            OtherProcessRunning = !createdNew;
+        }
+
+        #region IDisposable
+        public void Dispose()
+        {
+            if (m_mutex != null)
+            {
+                m_mutex.Dispose();
+                m_mutex = null;
+            }
+        }
+        #endregion
+    }
+}
