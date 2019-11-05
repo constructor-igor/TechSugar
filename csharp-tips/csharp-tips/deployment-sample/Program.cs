abba9e0e7449566491869443dd206400934225fd
constructor-igor <igor.zc@gmail.com>
2019-11-05 18:54:49 +0200

    #447

---
+++ csharp-tips/csharp-tips/deployment-sample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Application;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ *
+ *  https://docs.microsoft.com/en-us/dotnet/api/system.deployment.application.applicationdeployment.updatelocation?redirectedfrom=MSDN&view=netframework-4.8#System_Deployment_Application_ApplicationDeployment_UpdateLocation
+ */
+
+namespace deployment_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Version version = getRunningVersion();
+            Console.WriteLine($"Version: {version}");
+            Console.Write("press <Enter>");
+            Console.ReadLine();
+        }
+        private static Version getRunningVersion()
+        {
+            try
+            {
+                return ApplicationDeployment.CurrentDeployment.CurrentVersion;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Assembly.GetExecutingAssembly().GetName().Version;
+            }
+        }
+    }
+}
