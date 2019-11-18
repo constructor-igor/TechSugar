b83c242310cfb752f00becdca1409dc4d00802c0
constructor-igor <igor.zc@gmail.com>
2019-11-18 12:40:10 +0200

    added sample System.Diagnostics.Process.Start

---
+++ csharp-tips/csharp-tips/ProcessStartCmd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using NFile.Framework;
+
+namespace ProcessStartCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TemporaryFile file = new TemporaryFile("test.unknown"))
+            {
+                File.WriteAllText(file.FileName,"4;t");
+                System.Diagnostics.Process.Start(file.FileName);
+                Console.WriteLine("Press <Enter> to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
