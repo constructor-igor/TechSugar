9d89505a9226071796ca1a069f4e29c3732f5fa8
Igor Ziselman <igor.zc@gmail.com>
2023-07-03 16:11:29 +0300

    added sample WatchdogDemo

---
+++ WatchdogDemo/CustomProcess/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace TimeDisplayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time Display Application");
+            Console.WriteLine("Press any key to stop.");
+
+            // Start a new thread to display time every 10 seconds
+            Thread timeDisplayThread = new Thread(DisplayTime);
+            timeDisplayThread.Start();
+
+            // Wait for user input to stop the program
+            Console.ReadKey();
+
+            // Stop the time display thread gracefully
+            timeDisplayThread.Abort();
+        }
+
+        static void DisplayTime()
+        {
+            try
+            {
+                while (true)
+                {
+                    // Get the current time and display it
+                    DateTime currentTime = DateTime.Now;
+                    Console.WriteLine("Current Time: " + currentTime.ToString("HH:mm:ss"));
+
+                    // Pause the thread for 5 seconds
+                    Thread.Sleep(5000);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                // ThreadAbortException will be thrown when we call thread.Abort()
+                // We can ignore it in this case since we just want to stop the thread gracefully
+            }
+        }
+    }
+}
---
+++ WatchdogDemo/WatchDog/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WatchdogApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Watchdog Application");
+            Console.WriteLine("Press any key to stop.");
+
+            // Specify the path to the custom program you want to run
+            string programPath = @"D:\ig\myprojects\WatchdogDemo\Binaries\\CustomProcess.exe";
+
+            // Create a new thread to run the custom program
+            Thread programThread = new Thread(() => RunProgram(programPath));
+            programThread.Start();
+
+            // Wait for user input to stop the watchdog
+            Console.ReadKey();
+
+            // Stop the program thread gracefully
+            programThread.Abort();
+        }
+
+        static void RunProgram(string programPath)
+        {
+            while (true)
+            {
+                try
+                {
+                    // Create a new process for the custom program
+                    Process programProcess = new Process();
+                    programProcess.StartInfo.FileName = programPath;
+
+                    // Set the StartInfo properties to run the program in a separate console window
+                    programProcess.StartInfo.UseShellExecute = true;
+                    programProcess.StartInfo.CreateNoWindow = false;
+                    programProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+
+
+                    // Start the program
+                    programProcess.Start();
+
+                    // Wait for the program to finish
+                    programProcess.WaitForExit();
+
+                    // Program has finished, restart it after a delay
+                    Console.WriteLine("Program finished. Restarting in 5 seconds...");
+                    Thread.Sleep(5000);
+                }
+                catch (ThreadAbortException)
+                {
+                    // ThreadAbortException will be thrown when we call thread.Abort()
+                    // We can ignore it in this case since we just want to stop the thread gracefully
+                }
+                catch (Exception ex)
+                {
+                    // Handle any other exceptions that might occur during program execution
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                    // Optionally, you can choose to break out of the loop or implement a retry mechanism here.
+                    break;
+                }
+            }
+        }
+    }
+}
