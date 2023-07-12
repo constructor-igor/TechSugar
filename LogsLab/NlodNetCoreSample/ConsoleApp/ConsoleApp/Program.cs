ff267e487e0b88099edd9062a50eb1ed2b124884
Igor Ziselman <igor.zc@gmail.com>
2023-07-12 14:30:49 +0300

    added nlog sample for .net coere

---
+++ LogsLab/NlodNetCoreSample/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿//
+// https://github.com/NLog/NLog/wiki/Tutorial#Configure-NLog-Targets-for-output
+//
+public static class Program
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public static void Main()
+    {
+        try
+        {
+            Logger.Info("Hello world");
+            System.Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex, "Goodbye cruel world");
+        }
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
